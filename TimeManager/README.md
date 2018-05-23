83e40305511e5af0d2d0057d2e30e56bfc3e4af3
wuyu616818 <wuyu616818@163.com>
2018-05-23 20:12:16 +0800

    页面添加

--- TimeManager/README.md
+++ TimeManager/README.md
@@ -1,1 +1,4 @@
 ### :point_right: This starter repo has moved to the [ionic-team/starters](https://github.com/ionic-team/starters/tree/master/ionic-angular/official/tutorial) repo! :point_left:
+
+#如果动态修改代码遭遇Error: read ECONNRESET问题
+##删除node_modules/ws目录，然后在项目目录启动命令行，输入npm install ws@3.3.2
---
+++ TimeManager/src/app/route.module.ts
@@ -0,0 +1,18 @@
+// import { NgModule } from "@angular/core";
+// import {IonicPageModule} from "ionic-angular";
+// import {RegisterPage} from "../pages/register/register";
+// import {LoginPage} from "../pages/login/login";
+//
+// @NgModule({
+//   declarations: [
+//     LoginPage,
+//     RegisterPage,
+//   ],
+//   imports: [
+//     IonicPageModule.forChild(LoginPage),
+//     IonicPageModule.forChild(RegisterPage),
+//   ],
+// })
+// export class RouteModule {
+//
+// }
