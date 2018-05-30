ce1a853d582c1936644ded11570868d587703856
wuyu616818 <wuyu616818@163.com>
2018-05-30 19:18:25 +0800

    修改密码页面

---
+++ TimeManager/src/pages/user-forget-password/user-forget-password.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UserForgetPasswordPage } from './user-forget-password';
+
+@NgModule({
+  declarations: [
+    UserForgetPasswordPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UserForgetPasswordPage),
+  ],
+})
+export class UserForgetPasswordPageModule {}
