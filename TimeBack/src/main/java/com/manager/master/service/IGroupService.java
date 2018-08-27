9972eb19db29a33ac71fbaa48d5a18726b942bac
hujiadong <1826022177@qq.com>
2018-08-27 19:21:14 +0800

    查询群组接口

--- TimeBack/src/main/java/com/manager/master/service/IGroupService.java
+++ TimeBack/src/main/java/com/manager/master/service/IGroupService.java
@@ -1,11 +1,16 @@
 package com.manager.master.service;
 
+import com.manager.master.entity.GtdGroupEntity;
+import com.manager.master.repository.GroupJpaRepository;
+
+import java.util.List;
+
 /**
  * 群组Service
  *
  * create by wzy on 2018/08/24
  */
 public interface IGroupService {
-
-
+    List<GtdGroupEntity> findAll();
+    List<GtdGroupEntity> findByName(String name);
 }
--- TimeBack/src/main/java/com/manager/master/service/serviceImpl/GroupServicelmpl.java
+++ TimeBack/src/main/java/com/manager/master/service/serviceImpl/GroupServicelmpl.java
@@ -1,8 +1,13 @@
 package com.manager.master.service.serviceImpl;
 
+import com.manager.master.entity.GtdGroupEntity;
+import com.manager.master.repository.GroupJpaRepository;
 import com.manager.master.service.IGroupService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 群组Service实现类
@@ -12,6 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class GroupServicelmpl implements IGroupService {
+    @Resource
+    private GroupJpaRepository groupJpaRepository;
 
 
+    @Override
+    public List<GtdGroupEntity> findAll() {
+        return groupJpaRepository.findAll();
+    }
+
+    @Override
+    public List<GtdGroupEntity> findByName(String name) {
+        return groupJpaRepository.findByGroupName(name);
+    }
 }
