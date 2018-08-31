1b48530408154bf3de121adf0c18d1238e075256
17638848830 <h854300825@163.com>
2018-08-31 16:47:58 +0800

    群组列表、群组详情静态页面

--- TimeApp/src/pages/group-detail/group-detail.ts
+++ TimeApp/src/pages/group-detail/group-detail.ts
@@ -15,18 +15,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GroupDetailPage {
 
+  data1: object = {
+    groupId:1,
+    groupName:'开发一组',
+    groupLabel:[//标签
+      {
+        labelId:1,
+        labelName:'工作',
+      },{
+        labelId:2,
+        labelName:'生活',
+      },{
+        labelId:3,
+        labelName:'娱乐',
+      }
+    ],
+    gtdGroupMember:[//群成员
+      {
+        userId:1,
+        userName:'张三',
+        userContact:'17712341234'
+      },{
+        userId:2,
+        userName:'李四',
+        userContact:'17712345677'
+      },{
+        userId:3,
+        userName:'王五',
+        userContact:'18712345678'
+      },{
+        userId:4,
+        userName:'赵六',
+        userContact:'17612345678'
+      }
+    ]
+  }
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GroupDetailPage');
+    console.log(this.data1);
   }
 
   editgroup(){
-    let data: object = {
-      username:'张三sansansansansansansan'
-    };
-    this.navCtrl.push('GroupEditPage',data)
+    this.navCtrl.push('GroupEditPage',this.data1)
   }
 
 }
--- TimeApp/src/pages/group-list/group-list.ts
+++ TimeApp/src/pages/group-list/group-list.ts
@@ -14,16 +14,92 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'group-list.html',
 })
 export class GroupListPage {
-
+  data22:object;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.data22 = [
+      {
+        labelId:2,
+        qunzu:{
+          groupList:[{
+            groupId:1,
+						groupName:'开发1组',
+            groupLabel:[{
+              labelId:1,
+              labelName:'工作',
+              labelType:2,
+              userId:2,
+              userName:'李四',
+              userContact:'13255876092'
+            },{
+              labelId:1,
+              labelName:'生活',
+              labelType:2,
+              userId:2,
+              userName:'李四',
+              userContact:'13255876092'
+            },{
+              labelId:1,
+              labelName:'娱乐',
+              labelType:2,
+              userId:2,
+              userName:'李四',
+              userContact:'13255876092'
+            }]
+          },{
+            groupId:1,
+            groupName:'开发2组',
+            groupLabel:[{
+              labelId:1,
+              labelName:'工作',
+              labelType:2,
+              userId:2,
+              userName:'李四',
+              userContact:'13255876092'
+            },{
+              labelId:1,
+              labelName:'生活',
+              labelType:2,
+              userId:2,
+              userName:'李四',
+              userContact:'13255876092'
+            },{
+              labelId:1,
+              labelName:'娱乐',
+              labelType:2,
+              userId:2,
+              userName:'李四',
+              userContact:'13255876092'
+            }]
+          }]
+        }
+      },
+      {
+        labelId:8,
+        qunzu:{
+          groupList:[{
+            groupId:1,
+            groupName:'李四',
+            groupLabel:[{
+              labelId:1,
+              labelName:'娱乐',
+              labelType:2,
+              userId:4,
+              userName:'赵六',
+              userContact:'13255876092'
+            }]
+          }]
+        }
+      }
+    ]
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GroupListPage');
   }
 
-  xiangqing(){
-    this.navCtrl.push('GroupDetailPage')
+  xiangqing(groupORindividual){
+    console.log(groupORindividual)
+    this.navCtrl.push('GroupDetailPage',groupORindividual)
   }
 
   addORedit(){
