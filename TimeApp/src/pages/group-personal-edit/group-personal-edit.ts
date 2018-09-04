280508e463b4f262d02abf3acca9c1f003916e4f
17638848830 <h854300825@163.com>
2018-09-04 20:13:25 +0800

    查询详情数据对接

--- TimeApp/src/pages/group-personal-edit/group-personal-edit.ts
+++ TimeApp/src/pages/group-personal-edit/group-personal-edit.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
+import {AppConfig} from "../../app/app.config";
+import { HttpClient } from "@angular/common/http";
 
 /**
  * Generated class for the GroupEditPage page.
@@ -14,12 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'group-personal-edit.html',
 })
 export class GroupPersonalEditPage {
-
-  data3: string;
+  groupId:number;
+  data3: any;
   isEdit: boolean = true;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.data3 = navParams.data.groupId;
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient, public loadingCtrl: LoadingController) {
+    this.groupId = navParams.data.groupId;
+    this.http.post(AppConfig.GROUP_FIND_SINGLE_URL,{
+      userId:2,
+      groupId:this.groupId
+    }).subscribe(data => {
+      this.data3 = data
+      console.log(this.data3);
+      let loader = this.loadingCtrl.create({
+        content: this.data3.message,
+        duration: 1500
+      });
+      if (this.data3.code == "0") {
+        loader.present();
+        // this.navCtrl.push('HomePage');
+      } else {
+        loader.present();
+      }
+    })
   }
 
   ionViewDidLoad() {
