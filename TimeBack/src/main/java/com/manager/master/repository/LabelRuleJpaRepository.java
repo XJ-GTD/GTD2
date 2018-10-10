726592c6015edeaac80daaebf9767c12fa09b4ac
hujiadong <1826022177@qq.com>
2018-10-10 12:20:16 +0800

    退出群组，删除群组

--- TimeBack/src/main/java/com/manager/master/repository/LabelRuleJpaRepository.java
+++ TimeBack/src/main/java/com/manager/master/repository/LabelRuleJpaRepository.java
@@ -2,7 +2,10 @@ package com.manager.master.repository;
 
 import com.manager.master.entity.GtdLabelRuleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LabelRuleJpaRepository extends JpaRepository<GtdLabelRuleEntity,Integer> {
 
+    @Query(value = "SELECT IFNULL(LABEL_RULE_ID,0) FROM gtd_label_rule WHERE LABEL_ID=?1",nativeQuery = true)
+    Integer getIdByLabel(int labelId);
 }
