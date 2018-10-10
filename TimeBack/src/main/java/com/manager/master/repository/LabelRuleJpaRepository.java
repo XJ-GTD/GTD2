34e711b6ac848b9d4f3aece89fb350f88e6e6d0f
hujiadong <1826022177@qq.com>
2018-10-10 12:32:54 +0800

    判断标签是否为权限标签

--- TimeBack/src/main/java/com/manager/master/repository/LabelRuleJpaRepository.java
+++ TimeBack/src/main/java/com/manager/master/repository/LabelRuleJpaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface LabelRuleJpaRepository extends JpaRepository<GtdLabelRuleEntity,Integer> {
 
-    @Query(value = "SELECT IFNULL(LABEL_RULE_ID,0) FROM gtd_label_rule WHERE LABEL_ID=?1",nativeQuery = true)
+    @Query(value = "SELECT LABEL_RULE_ID FROM gtd_label_rule WHERE LABEL_ID=?1",nativeQuery = true)
     Integer getIdByLabel(int labelId);
 }
