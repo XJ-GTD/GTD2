352febe1c46ad2c89b9edb6c8cbcb0b7e2fc9471
jy-zhang <jy-zhang@zakj.info>
2018-10-11 19:23:28 +0800

    Merge branch 'master' of https://github.com/XJ-GTD/GTD2
    # Conflicts:
    #	TimeApp/src/pages/home/home.ts

--- TimeApp/src/pages/home/home.ts
+++ TimeApp/src/pages/home/home.ts
@@ -63,7 +63,6 @@ export class HomePage {
               private http: HttpClient,
               private timeService: TimeService,
               private paramsService: ParamsService,
-              private nativePageTransitions: NativePageTransitions,
               private alarmClock: XiaojiAlarmclockService) {
 
     this.init();
