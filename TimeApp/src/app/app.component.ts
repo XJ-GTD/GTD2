97f05e3618ca82874daa89c4af9f17774e9ed4ec
dingchaohui <ch-ding@zakj.info>
2018-12-10 14:29:15 +0800

    初始化创建数据库

--- TimeApp/src/app/app.component.ts
+++ TimeApp/src/app/app.component.ts
@@ -54,6 +54,62 @@ export class MyApp {
     });
   }
   init(){
+    //查询版本
+    this.fisqlite.getfi(1).then(data=>{
+      let istrue:boolean = false
+      if(data && data.rows && data.rows.length>0){
+        if(data.rows.item(0).isup==1){
+          istrue=true;
+          this.fisqlite.ufi(null,0)
+        }
+      }else{
+        istrue=true;
+        this.fisqlite.afi(1,0)
+      }
+      //如果发现最新更新则跳转引导页
+      if(istrue){
+        this.rootPage = PageConfig.AZ_PAGE;
+      }else{
+        //获取Token
+        this.user.getUo().then(data=>{
+          // AppConfig.Token=data.u.uT;
+          if(data && data.u && data.u.uT && data.u.uT !='null' && data.u.uT !=''){
+            AppConfig.Token=data.u.uT;
+            console.debug('MyApp初始化Token'+AppConfig.Token)
+          }
+          console.debug(JSON.stringify(data))
+        }).catch(e=>{
+          console.error("MyApp获取Token失败"+e.message)
+        })
+        this.rootPage = PageConfig.HZ_PAGE;
+      }
+      this.nav.setRoot(this.rootPage);
+    }).catch(e=>{
+      //首次打开App,初始化创建数据库建表
+      this.baseSqlite.createTable();
+      this.rootPage = PageConfig.AZ_PAGE;
+      this.nav.setRoot(this.rootPage);
+
+    })
+  }
+  ngAfterViewInit(){
+    //通过key，判断是否曾进入过引导页
+    // this.storage.get('firstIn').then((result) => {
+    //
+    //   if (result != null && result) {
+    //     this.rootPage = PageConfig.HZ_PAGE;
+    //   } else {
+    //     //初始化创建数据库建表
+    //     this.baseSqlite.createTable();
+    //     this.storage.set('firstIn', true);
+    //     this.rootPage = PageConfig.AZ_PAGE;
+    //   }
+    //
+    //   if (this.nav.getViews().length == 0){
+    //     this.nav.setRoot(this.rootPage);
+    //   }
+    //
+    // });
     //确保异步执行完后才隐藏启动动画
     this.events.subscribe('db:create', () => {
       //创建数据库与表成功后才关闭动画跳转页面
@@ -62,63 +118,6 @@ export class MyApp {
     })
     //初始化创建数据库
     this.baseSqlite.createDb();
-  }
-  ngAfterViewInit(){
-    //通过key，判断是否曾进入过引导页
-    this.storage.get('firstIn').then((result) => {
-
-      if (result != null && result) {
-        this.rootPage = PageConfig.HZ_PAGE;
-      } else {
-        //初始化创建数据库建表
-        this.baseSqlite.createTable();
-        this.storage.set('firstIn', true);
-        this.rootPage = PageConfig.AZ_PAGE;
-      }
-
-      if (this.nav.getViews().length == 0){
-        this.nav.setRoot(this.rootPage);
-      }
-
-    });
-
-    // //初始化创建数据库
-    // this.baseSqlite.createDb().then(data=>{
-    //   //查询版本
-    //   this.fisqlite.getfi(1).then(data=>{
-    //     let istrue:boolean = false
-    //     if(data && data.rows && data.rows>0){
-    //       if(data.rows.item(0).isup==1){
-    //         istrue=true;
-    //         this.fisqlite.ufi(null,0)
-    //       }
-    //     }else{
-    //       istrue=true;
-    //       this.fisqlite.afi(1,0)
-    //       this.user.getUo().then(data=>{
-    //         // AppConfig.Token=data.u.uT;
-    //         console.debug(JSON.stringify(data))
-    //       }).catch(e=>{
-    //         console.error(e.message)
-    //       })
-    //     }
-    //     //如果发现最新更新则跳转引导页
-    //     if(istrue){
-    //       this.rootPage = PageConfig.AZ_PAGE;
-    //     }else{
-    //       this.rootPage = PageConfig.HZ_PAGE;
-    //     }
-    //     this.nav.setRoot(this.rootPage);
-    //   }).catch(e=>{
-    //     this.rootPage = PageConfig.AZ_PAGE;
-    //     this.nav.setRoot(this.rootPage);
-    //     //初始化创建数据库建表
-    //     this.baseSqlite.createTable();
-    //   })
-    // }).catch(e=>{
-    //
-    // })
-
   }
 
 }
