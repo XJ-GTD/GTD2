c093f10b4ba6bde07ac930308fb38c9e61ecd525
dinggao <540685946@qq.com>
2018-12-24 15:43:05 +0800

    http请求修改

--- TimeApp/src/service/restful/bs-restful.ts
+++ TimeApp/src/service/restful/bs-restful.ts
@@ -11,7 +11,7 @@ import { HTTP } from '@ionic-native/http';
 export class BsRestful {
 
   constructor(private http:HTTP){
-    http.setDataSerializer("json")
+    http.setDataSerializer("json");
   }
   /**
    * http请求
@@ -20,17 +20,17 @@ export class BsRestful {
   post(url:string,body:any):Promise<any> {
     return new Promise((resolve, reject) => {
       if(DataConfig.uInfo && DataConfig.uInfo.uT){
-        AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT
-        console.log(url + "请求头Token：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON))
+        AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT;
+        console.log(url + "请求头Token：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON));
       }else{
-        console.error(url + "请求头Token未取到")
+        console.error(url + "请求头Token未取到");
       }
       this.http.post(url,body,{ "Authorization": DataConfig.uInfo.uT }).then(data=>{
        // data.data = JSON.parse(data.data)
-        resolve(JSON.parse(data.data))
+        resolve(JSON.parse(data.data));
       }).catch(e=>{
-          console.error(url + "请求头部：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON))
-          console.error(url + "请求报错：" + JSON.stringify(e))
+          console.error(url + "请求头部：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON));
+          console.error(url + "请求报错：" + JSON.stringify(e));
         reject(e);
       })
     })
