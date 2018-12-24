2bdf0049fa9653551817e6053b3941f9034e23ac
dingchaohui <ch-ding@zakj.info>
2018-12-25 00:18:55 +0800

    实例化修改

--- TimeApp/src/service/sync.service.ts
+++ TimeApp/src/service/sync.service.ts
@@ -18,7 +18,7 @@ import {DataConfig} from "../app/data.config";
 @Injectable()
 export class SyncService {
 
-  constructor( private lbSqlite:LbSqlite,private sync:SyncRestful) {
+  constructor( private base:BaseSqlite,private sync:SyncRestful) {
 
   }
 
@@ -33,8 +33,46 @@ export class SyncService {
         .then(data => {
           console.log("-------SyncService initzdlb restful 初始化字典数据及标签表接口返回结果："+JSON.stringify(data))
           base = data
+          if(data.code == 0 && data.syncDataList.length>0){
+            let sql=''
+            for(let a=0;a<data.syncDataList.length;a++){
+              let res = data.syncDataList[1];
+              //字典类型表
+              if(res.type=='dictionary' && res.dataList.length>0){
+                for(let i=0;i<data.syncDataList.length;i++){
+                  let dict = data.syncDataList[i];
+                  let str = 'key'
+                  sql=sql+ 'insert into GTD_X(zt,zv) values("'+ dict.key+'","'+ dict.value+'");';
+                }
+              }
+              //字典数据表
+              if(res.type=='dictionaryData' && res.dataList.length>0){
+                for(let i=0;i<data.syncDataList.length;i++){
+                  let dict = data.syncDataList[i];
+                  sql=sql+ 'insert into GTD_Y(zt,zk,zkv,px) values("'+ dict.type+'","'+ dict.key+'","'+ dict.value+'",'+ dict.id+');';
+                }
+              }
+
+              //标签表
+              if(res.type=='label' && res.dataList.length>0){
+                for(let i=0;i<data.syncDataList.length;i++){
+                  let dict = data.syncDataList[i];
+                  sql=sql+ 'insert into GTD_F(lai,lan,lat) values("'+ dict.id+'","'+ dict.value+'","'+ dict.type+'");';
+                }
+              }
+            }
+
+            if(sql != null){
+              console.log("------SyncService initzdlb insert into table  --------")
+              return this.base.importSqlToDb(sql);
+            }
+          }
+        }).then(data=>{
+          if(base.code==0 && base.data != null){
+            console.log("------SyncService initzdlb insert into table End : " + JSON.stringify(data));
+          }
           resolve(base)
-        }).catch(e=>{
+      }).catch(e=>{
         console.log("-------SyncService initzdlb restful 初始化字典数据及标签表 Error："+JSON.stringify(e))
         base.message = e.message
         base.code = 1
