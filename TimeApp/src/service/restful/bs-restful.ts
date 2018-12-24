bb51e745c9bc8440da8cfee86a7bf558fe24793c
wuyu616818 <wuyu616818@163.com>
2018-12-24 16:11:16 +0800

    文档更新

--- TimeApp/src/service/restful/bs-restful.ts
+++ TimeApp/src/service/restful/bs-restful.ts
@@ -17,20 +17,20 @@ export class BsRestful {
    * http请求
    * @param {string} am 手机号
    */
-  post(url:string,body:any):Promise<any> {
+  post(url:string, body:any):Promise<any> {
     return new Promise((resolve, reject) => {
       if(DataConfig.uInfo && DataConfig.uInfo.uT){
-        AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT
-        console.log(url + "请求头Token：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON))
+        AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT;
+        console.log(url + "请求头Token：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON));
       }else{
         console.error(url + "请求头Token未取到")
       }
       this.http.post(url,body,{ "Authorization": DataConfig.uInfo.uT }).then(data=>{
        // data.data = JSON.parse(data.data)
-        resolve(JSON.parse(data.data))
+        resolve(JSON.parse(data.data));
       }).catch(e=>{
-          console.error(url + "请求头部：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON))
-          console.error(url + "请求报错：" + JSON.stringify(e))
+          console.error(url + "请求头部：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON));
+          console.error(url + "请求报错：" + JSON.stringify(e));
         reject(e);
       })
     })
