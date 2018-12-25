66126c9c72c3b2b0e5cda5c8dc598fbc0ce99a5c
dingchaohui <ch-ding@zakj.info>
2018-12-25 15:08:39 +0800

    初始化数据修改

--- TimeApp/src/service/restful/bs-restful.ts
+++ TimeApp/src/service/restful/bs-restful.ts
@@ -14,37 +14,26 @@ export class BsRestful {
     this.http.setDataSerializer("json");
     console.log("nocheck===================================================================");
     this.http.setSSLCertMode("nocheck").then(data=>{
-      console.log("----------- BsRestful setSSLCertMode Success ------------ ");
-      console.log("----------- BsRestful setSSLCertMode data ----- :" +  JSON.stringify(data))
+      console.log("----------- BsRestful setSSLCertMode Success : "  +  JSON.stringify(data));
     }).catch(err=>{
-      console.error("----------- BsRestful setSSLCertMode Error ------------ ");
-      console.error("----------- BsRestful setSSLCertMode Error data: " + JSON.stringify(err))
+      console.error("----------- BsRestful setSSLCertMode Error: " + JSON.stringify(err))
     });
   }
 
-  // public setHttpHeader(key:value){
-  //   // this.http.setHeader()
-  // }
   /**
    * http请求
    * @param {string} am 手机号
    */
   post(url:string, body:any):Promise<any> {
     return new Promise((resolve, reject) => {
-      if(DataConfig.uInfo && DataConfig.uInfo.uT){
-        AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT;
-        console.log(url + "请求头Token：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON));
-      }else{
-        console.error(url + "请求头Token未取到");
-      }
-
+      console.log("------ 开始请求（"+url + "）-------");
       this.http.post(url,body,{ "Authorization": DataConfig.uInfo.uT }).then(data=>{
-        let jsonData = JSON.parse(data.data)
+        let jsonData = JSON.parse(data.data);
         //获取返回值message
-        jsonData.message=ReturnConfig.RETURN_MSG.get(jsonData.code+"")
+        jsonData.message=ReturnConfig.RETURN_MSG.get(jsonData.code+"");
         resolve(jsonData);
       }).catch(e=>{
-          console.error(url + "请求头部：" + JSON.stringify(AppConfig.HEADER_OPTIONS_JSON));
+          console.error(url + "请求头部：" + DataConfig.uInfo.uT);
           console.error(url + "请求报错：" + JSON.stringify(e));
         reject(e);
       })
