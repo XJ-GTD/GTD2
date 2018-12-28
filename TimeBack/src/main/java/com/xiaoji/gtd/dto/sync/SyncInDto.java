e2004f37f7e3a4ef2e66884e46ed997a6c53719a
wuyu616818 <wuyu616818@163.com>
2018-12-28 17:55:22 +0800

    文档更新

--- TimeBack/src/main/java/com/xiaoji/gtd/dto/sync/SyncInDto.java
+++ TimeBack/src/main/java/com/xiaoji/gtd/dto/sync/SyncInDto.java
@@ -1,7 +1,48 @@
 package com.xiaoji.gtd.dto.sync;
 
+import java.util.List;
+
 /**
  * 同步数据入参类
+ *
+ * create by wzy on 2018/12/27
  */
 public class SyncInDto {
+
+    private String userId;
+    private String deviceId;
+    private String version;
+    private List<SyncDataDto> syncDataList;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<SyncDataDto> getSyncDataList() {
+        return syncDataList;
+    }
+
+    public void setSyncDataList(List<SyncDataDto> syncDataList) {
+        this.syncDataList = syncDataList;
+    }
 }
