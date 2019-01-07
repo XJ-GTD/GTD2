ad885b0baf6f0447fdc7d5e2cf1d6588be06f9d3
dinggao <540685946@qq.com>
2019-01-07 19:23:07 +0800

    sa

--- TimeApp/src/service/restful/bs-restful.ts
+++ TimeApp/src/service/restful/bs-restful.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { DataConfig } from "../../app/data.config";
 import { HTTP } from "@ionic-native/http";
 import { ReturnConfig } from "../../app/return.config";
-import {AppConfig} from "../../app/app.config";
-import {HttpClient} from "@angular/common/http";
 
 /**
  * 基础resful请求
@@ -11,7 +9,7 @@ import {HttpClient} from "@angular/common/http";
 @Injectable()
 export class BsRestful {
 
-  constructor(public http: HTTP,private httpClient: HttpClient){
+  constructor(public http: HTTP){
 
   }
 
@@ -32,7 +30,7 @@ export class BsRestful {
   post(url:string, body:any):Promise<any> {
     return new Promise((resolve, reject) => {
       console.log("------ 开始请求（"+url + "）-------");
-      if(DataConfig.IS_MOBILE){
+      // if(DataConfig.IS_MOBILE){
         this.http.post(url,body,{ "Authorization": DataConfig.uInfo.uT }).then(data=>{
           let jsonData = JSON.parse(data.data);
           //获取返回值message
@@ -43,16 +41,16 @@ export class BsRestful {
           console.error(url + "请求报错：" + JSON.stringify(e));
           reject(e);
         })
-      }else{
-        AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT;
-        this.httpClient.post(url,body,AppConfig.HEADER_OPTIONS_JSON).subscribe(data=>{
-          resolve(data)
-        },err => {
-          console.error(url + "请求头部：" + DataConfig.uInfo.uT);
-          console.error(url + "请求报错：" + JSON.stringify(err));
-          reject(err)
-        })
-      }
+      // }else{
+      //   AppConfig.HEADER_OPTIONS_JSON.headers.Authorization=DataConfig.uInfo.uT;
+      //   this.httpClient.post(url,body,AppConfig.HEADER_OPTIONS_JSON).subscribe(data=>{
+      //     resolve(data)
+      //   },err => {
+      //     console.error(url + "请求头部：" + DataConfig.uInfo.uT);
+      //     console.error(url + "请求报错：" + JSON.stringify(err));
+      //     reject(err)
+      //   })
+      // }
     })
   }
 }
