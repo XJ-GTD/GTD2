268d5adc11479d7f76128714ae3337eeb38dd690
dingchaohui <ch-ding@zakj.info>
2019-01-11 16:50:24 +0800

    sync

--- TimeApp/src/service/util-service/util.service.ts
+++ TimeApp/src/service/util-service/util.service.ts
@@ -26,7 +26,7 @@ export class UtilService {
    * @returns {string}
    */
   public getUuid() {
-    let CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    let CHARS = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
 
     let uuid = function (len, radix) {
       var chars = CHARS, uuid = [], i;
