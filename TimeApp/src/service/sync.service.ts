ac6709e11fde2df2d4552e3302118f6517890a58
dingchaohui <ch-ding@zakj.info>
2019-01-13 17:11:22 +0800

    sync

--- TimeApp/src/service/sync.service.ts
+++ TimeApp/src/service/sync.service.ts
@@ -233,7 +233,7 @@ export class SyncService {
         let sdl = [];
         if(data && data.rows && data.rows.length>0){
           bv = data.rows.item(0).id;
-          for(let tn in DataConfig.TABLE_NAMES){
+          for(let tn of DataConfig.TABLE_NAMES){
             let sd:any = {};
             sd.tableName = tn;
             sd.dataList=[];
