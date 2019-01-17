5d828c3026e33e70caea2c66b95bf1af5ca8235f
zhangjy <jy-zhang@zakj.info>
2019-01-17 19:54:43 +0800

    语音播报停止功能

--- BaiduSpeechAndTTS/www/XjBaiduTts.js
+++ BaiduSpeechAndTTS/www/XjBaiduTts.js
@@ -4,6 +4,9 @@ var exec = require('cordova/exec');
 var xjBaiduTtsApi = {
     startSpeak:function(success,error,speakMessage){
         exec(success,error,"XjBaiduTts","speak",[speakMessage]);
+    },
+    speakStop:function(success,error,speakMessage){
+        exec(success,error,"XjBaiduTts","stop",[]);
     }
 };
 
