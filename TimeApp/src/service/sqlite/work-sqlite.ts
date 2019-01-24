de82b3f327b3e2fb86d4a5fe7e98c83e40d254d6
dinggao <540685946@qq.com>
2019-01-24 22:14:32 +0800

    sa

--- TimeApp/src/service/sqlite/work-sqlite.ts
+++ TimeApp/src/service/sqlite/work-sqlite.ts
@@ -193,7 +193,7 @@ export class WorkSqlite{
           //判断是否有消息
           for(let j=0;j<=data.rows.length;j++){
             let bool = true; //判断当前日是否存在
-            for(let i=0;i<=resL.length;i++){
+            for(let i=0;i<resL.length;i++){
               if(resL[i].ymd== data.rows.item(j).md){
                 resL[i].mdn=1;
                 bool = false;
