70710b9ecb420b3555a6cca1e7665fbdc2fd06c6
dingchaohui <ch-ding@zakj.info>
2019-01-31 11:36:18 +0800

    日程子表修改

--- TimeApp/src/service/work.service.ts
+++ TimeApp/src/service/work.service.ts
@@ -803,7 +803,8 @@ export class WorkService {
           sd.comment=data.rm;
           sd.repeatType=data.cft;
           sd.remindTime=data.ac;
-          sd.scheduleStartTime=data.sd;
+          sd.scheduleStartTime=data.sd.substr(12,5);
+          sd.scheduleStartDate=data.sd.substr(0,10);
           sd.scheduleDeadline=data.ed;
           sd.scheduleStatus = data.fh;
           sd.scheduleFinishDate=data.wd;
