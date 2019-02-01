b7661f6132f575fc149c023198cd1a545a563bbc
zhangjy <jy-zhang@zakj.info>
2019-02-01 12:38:41 +0800

    Merge branch 'develop' of github.com:XJ-GTD/GTD2 into develop
    # Conflicts:
    #	TimeApp/src/pages/ha/ha.ts

--- TimeApp/src/pages/sb/sb.module.ts
+++ TimeApp/src/pages/sb/sb.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SbPage } from './sb';
+import { ElModule } from 'element-angular'
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { SbPage } from './sb';
   ],
   imports: [
     IonicPageModule.forChild(SbPage),
+    ElModule
   ],
 })
 export class SbPageModule {}
