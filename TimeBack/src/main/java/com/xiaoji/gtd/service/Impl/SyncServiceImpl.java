f9e9f3f7ff73eb165ff795b0a693f354c2d77628
Leon <leon_xi@163.com>
2019-02-13 14:17:40 +0800

    增加下载数据执行时间

--- TimeBack/src/main/java/com/xiaoji/gtd/service/Impl/SyncServiceImpl.java
+++ TimeBack/src/main/java/com/xiaoji/gtd/service/Impl/SyncServiceImpl.java
@@ -581,6 +581,8 @@ public class SyncServiceImpl implements ISyncService {
      * 下载数据
      */
     private List<SyncDataDto> downLoad(String userId, String deviceId, String version, String downloadSyncVersion) {
+        long start = System.currentTimeMillis();
+
         List<SyncDataDto> syncDataList = new ArrayList<>();
         SyncDataDto syncData;
         List<SyncTableData> dataList;
@@ -1012,7 +1014,9 @@ public class SyncServiceImpl implements ISyncService {
             logger.error("查询下载数据异常", e); // 席理加修改
             return null;
         }
-
+        long cost = start - System.currentTimeMillis();
+        logger.debug("DEBUG++ download cost time " + cost / 1000);
+        
         return syncDataList;
     }
 
