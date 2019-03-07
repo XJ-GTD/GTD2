33abd2f91dc941f381a55b2cae4ab52eb1df0eee
shopping <xuzp@linkstec.com>
2019-03-07 18:55:06 +0800

    db基类修改

--- TimeApp/src/service/sqlite/tbl/faction.db.ts
+++ TimeApp/src/service/sqlite/tbl/faction.db.ts
@@ -5,6 +5,9 @@ import {BsModel} from "../../../model/out/bs.model";
 import {SQLitePorter} from "@ionic-native/sqlite-porter";
 import {UtilService} from "../../util-service/util.service";
 import {Events} from "ionic-angular";
+import {UEntity} from "../../../entity/u.entity";
+import {FiEntity} from "../../../entity/fi.entity";
+import {SyvEntity} from "../../../entity/syv.entity";
 
 /**
  * create by on 2019/3/5
@@ -33,7 +36,7 @@ export class FactionDb{
   /**
    * 创建数据库
    */
-  createDb(): Promise<BsModel> {
+  generateDb(): Promise<BsModel> {
     return new Promise((resolve, reject) => {
       let bs = new BsModel();
       console.log( " start create Db mingWX.db")
@@ -59,5 +62,61 @@ export class FactionDb{
       }
       //alert('创建数据成功！')
     });
+  }
+
+  /**
+   * 初始化表数据
+   * @param {BsModel} data
+   * @returns {Promise<any>}
+   */
+  initDbData(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if(DataConfig.isFirst!=0){
+        console.log("-------------------BaseSqlite initData table  data to start ------------------")
+        //版本表
+        let fi = new FiEntity();
+        fi.id = 1;
+        fi.firstIn = 1;
+        fi.isup = 1;
+        //用户表
+        let u: UEntity = new UEntity();
+        u.uI = this.util.getUuid();
+        u.uty = '0';
+        let syv = new SyvEntity();
+        syv.si=1;
+        syv.bv=0;
+        syv.fv='0';
+        if(DataConfig.IS_MOBILE){
+          //手机端
+          let sql=fi.isq+u.isq+syv.isq;
+          this.importSqlToDb(sql).then(data=>{
+            console.log("-------------------BaseSqlite initData  GTD_A and GTD_FI table to data: "+JSON.stringify(data))
+            resolve(data)
+          }).catch(e=>{
+            console.error("------------------BaseSqlite initData to table data: "+e.message);
+            reject(e);
+          })
+        }else{
+          //web端
+          this.save(u).then(data=>{
+            console.log("-------------------BaseSqlite initData GTD_A to table data: "+JSON.stringify(data))
+            return this.save(fi);
+          }).then(data=>{
+            console.log("-------------------BaseSqlite initData GTD_A to table data: "+JSON.stringify(data))
+            return this.save(syv);
+          }).then(data=>{
+            console.log("-------------------BaseSqlite initData GTD_FI to table data: "+JSON.stringify(data))
+            resolve(data);
+          }).catch(e=>{
+            console.error("------------------BaseSqlite createTable: "+e.message)
+            reject(e);
+          })
+        }
+      }else{
+        console.log("-------------------BaseSqlite initData table data is exsit ------------------")
+        let bs = new BsModel();
+        resolve(bs);
+      }
+    })
   }
 }
