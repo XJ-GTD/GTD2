33c3756fff351da63c535bceafdcf423269490d4
PenyB <1014113078@qq.com>
2019-03-08 12:55:28 +0800

    tbl

--- TimeApp/src/service/sqlite/tbl/jh.tbl.ts
+++ TimeApp/src/service/sqlite/tbl/jh.tbl.ts
@@ -1,87 +1,7 @@
-import{Injectable}from'@angular/core';
-import {SqliteExec} from "../../util-service/sqlite.exec";
-
 /**
  * create by on 2019/3/5
  */
-@Injectable()
 export class JhTbl  {
-
-  constructor( private sqlExec: SqliteExec ){
-
-  }
-
-  cT():Promise<any> {
-
-    let sq ='CREATE TABLE IF NOT EXISTS GTD_J_H(  ji VARCHAR(50) PRIMARY KEY ,jn VARCHAR(100)  ,jg VARCHAR(100)' +
-      ',jc VARCHAR(10));';
-
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  upT(pro:JhPro):Promise<any> {
-    let sq='update GTD_J_H set 1=1 ';
-    if(pro.jn!=null){
-      sq=sq+', jn="' + pro.jn +'"';
-    }
-    if(pro.jg!=null){
-      sq=sq+', jg="' + pro.jg +'"';
-    }
-    if(pro.jc!=null){
-      sq=sq+', jc="' + pro.jc +'"';
-    }
-    sq = sq + ' where ji = "'+ pro.ji +'"';
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  dT(pro:JhPro):Promise<any> {
-    let sq = 'delete from GTD_J_H where ji = "' + pro.ji +'"';
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  sloT(pro:JhPro):Promise<any> {
-    let sq='select * from GTD_J_H where ji = "'+ pro.ji +'"';
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  slT(pro:JhPro):Promise<any> {
-    let sq='select * from  GTD_J_H where  1=1 ';
-    if(pro.jn!=null){
-      sq=sq+' and jn="' + pro.jn +'"';
-    }
-    if(pro.jg!=null){
-      sq=sq+' and jg="' + pro.jg +'"';
-    }
-    if(pro.jc!=null){
-      sq=sq+' and jc="' + pro.jc +'"';
-    }
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  drT():Promise<any> {
-
-    let sq ='DROP TABLE IF EXISTS GTD_J_H;';
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  inT(pro:JhPro):Promise<any> {
-    let sq ='insert into GTD_J_H ' +
-      '(  ji ,jn ,jg,jc) values("'+ pro.ji+'","'+ pro.jn+'","'+pro.jg+ '","'+pro.jc+ '")';
-
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-  rpT(pro:JhPro):Promise<any> {
-    let sq ='replace into GTD_J_H ' +
-      '(  ji ,jn ,jg,jc) values("'+ pro.ji+'","'+ pro.jn+'","'+pro.jg+ '","'+pro.jc+ '")';
-
-    return this.sqlExec.execSql(sq,[]);
-  }
-
-}
-
-class JhPro{
-
   private _ji: string;
   private _jn: string;
   private _jg: string;
@@ -125,5 +45,71 @@ class JhPro{
     this._jg = null;
   };
 
-}
+  cT():string{
 
+    let sq ='CREATE TABLE IF NOT EXISTS GTD_J_H(  ji VARCHAR(50) PRIMARY KEY ,jn VARCHAR(100)  ,jg VARCHAR(100)' +
+      ',jc VARCHAR(10));';
+
+    return sq;
+  }
+
+  upT():string{
+    let sq='update GTD_J_H set 1=1 ';
+    if(this._jn!=null){
+      sq=sq+', jn="' + this._jn +'"';
+    }
+    if(this._jg!=null){
+      sq=sq+', jg="' + this._jg +'"';
+    }
+    if(this._jc!=null){
+      sq=sq+', jc="' + this._jc +'"';
+    }
+    sq = sq + ' where ji = "'+ this._ji +'"';
+    return sq;
+  }
+
+  dT():string{
+    let sq = 'delete from GTD_J_H where ji = "' + this._ji +'"';
+    return sq;
+  }
+
+  sloT():string{
+    let sq='select * from GTD_J_H where ji = "'+ this._ji +'"';
+    return sq;
+  }
+
+  slT():string{
+    let sq='select * from  GTD_J_H where  1=1 ';
+    if(this._jn!=null){
+      sq=sq+' and jn="' + this._jn +'"';
+    }
+    if(this._jg!=null){
+      sq=sq+' and jg="' + this._jg +'"';
+    }
+    if(this._jc!=null){
+      sq=sq+' and jc="' + this._jc +'"';
+    }
+    return sq;
+  }
+
+  drT():string{
+
+    let sq ='DROP TABLE IF EXISTS GTD_J_H;';
+    return sq;
+  }
+
+  inT():string{
+    let sq ='insert into GTD_J_H ' +
+      '(  ji ,jn ,jg,jc) values("'+ this._ji+'","'+ this._jn+'","'+this._jg+ '","'+this._jc+ '")';
+
+    return sq;
+  }
+
+  rpT():string{
+    let sq ='replace into GTD_J_H ' +
+      '(  ji ,jn ,jg,jc) values("'+ this._ji+'","'+ this._jn+'","'+this._jg+ '","'+this._jc+ '")';
+
+    return sq;
+  }
+
+}
