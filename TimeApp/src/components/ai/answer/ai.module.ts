28c30081a08320ab2be5776f9eb1c3a84da729f3
PenyB <1014113078@qq.com>
2019-03-11 19:06:56 +0800

    ai

--- TimeApp/src/components/ai/answer/ai.module.ts
+++ TimeApp/src/components/ai/answer/ai.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HbPage } from './ai';
-import { ParamsService } from "../../../service/util-service/params.service";
-import { XiaojiAssistantService } from "../../../service/cordova/assistant.service";
-import { XiaojiFeedbackService } from "../../../service/cordova/feedback.service";
+import { AssistantService } from "../../../service/cordova/assistant.service";
+import { FeedbackService } from "../../../service/cordova/feedback.service";
 import {AiService} from "./ai.service";
 
 @NgModule({
@@ -14,9 +13,8 @@ import {AiService} from "./ai.service";
     IonicPageModule.forChild(HbPage),
   ],
   providers: [
-    ParamsService,
-    XiaojiAssistantService,
-    XiaojiFeedbackService,
+    AssistantService,
+    FeedbackService,
     AiService,
   ],
   entryComponents: [
