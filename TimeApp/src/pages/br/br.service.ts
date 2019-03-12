0471973c2361f218ed6d66702d56bf8c1db55901
shopping <xuzp@linkstec.com>
2019-03-12 19:14:03 +0800

    备份页面service

--- TimeApp/src/pages/br/br.service.ts
+++ TimeApp/src/pages/br/br.service.ts
@@ -1,34 +1,80 @@
 import {Injectable} from "@angular/core";
+import {CTbl} from "../../service/sqlite/tbl/c.tbl";
+import {BackupPro, BacRestful} from "../../service/restful/bacsev";
+import {SqliteExec} from "../../service/util-service/sqlite.exec";
+import {UserConfig} from "../../service/config/user.config";
+import {SpTbl} from "../../service/sqlite/tbl/sp.tbl";
+import {ETbl} from "../../service/sqlite/tbl/e.tbl";
+import {BTbl} from "../../service/sqlite/tbl/b.tbl";
+import {DTbl} from "../../service/sqlite/tbl/d.tbl";
+import {GTbl} from "../../service/sqlite/tbl/g.tbl";
+import {JhTbl} from "../../service/sqlite/tbl/jh.tbl";
+import {BxTbl} from "../../service/sqlite/tbl/bx.tbl";
 
 @Injectable()
 export class BrService {
-  constructor() {
+  constructor(private bacRestful: BacRestful,private sqlexec :SqliteExec) {
 
   }
 
   //备份方法，需要传入页面 ，画面显示备份进度条
-  async backup():Promise<any>{
+  async backup(): Promise<any> {
     //定义上传信息JSSON List
-      //获取本地日历
-      //获取特殊日历
-      //获取提醒数据
-      //获取日程参与人数据
-      //获取联系人信息
-      //获取群组信息
-      //获取本地计划
+
+    let backupPro: BackupPro = new BackupPro();
+    //操作账户ID
+    backupPro.oai="a13661617252"
+    //操作手机号码
+    backupPro.ompn="13661617252";
+    //时间戳
+    backupPro.d.bts=0;
+
+    //获取本地日历
+    let c = new CTbl();
+    backupPro.d.c = await this.sqlexec.getList(c);
+
+    //获取特殊日历
+    let sp = new SpTbl();
+    backupPro.d.sp = await this.sqlexec.getList(sp);
+
+    //获取提醒数据
+    let e = new ETbl();
+    backupPro.d.e = await this.sqlexec.getList(e);
+
+    //获取日程参与人数据
+    let d = new DTbl();
+    backupPro.d.d = await this.sqlexec.getList(d);
+
+    //获取联系人信息
+    let b = new BTbl();
+    backupPro.d.b = await this.sqlexec.getList(b);
+
+    //获取群组信息
+    let g = new GTbl();
+    backupPro.d.g = await this.sqlexec.getList(g);
+
+    //获取群组人员信息
+    let bx = new BxTbl();
+    backupPro.d.bx = await this.sqlexec.getList(bx);
+
+    //获取本地计划
+    let jh = new JhTbl();
+    backupPro.d.jh = await this.sqlexec.getList(jh);
 
     //restFul上传
+    await this.bacRestful.backup(backupPro);
+
     return null
 
   }
 
-  getCount():Promise<number>{
+  getCount(): Promise<number> {
     //restFul 获取服务器 日历条数
     return null
   }
 
 
-  recover():Promise<any>{
+  recover(): Promise<any> {
     //restFul 下载用户数据
 
 
@@ -47,9 +93,9 @@ export class BrService {
 }
 
 
-export class BrData{
+export class BrData {
   //日历list
-  a1:Array<any>
+  a1: Array<any>
   //提醒数据list。。。。。
 
 }
