1dc4a0db4b55151e12ad3b98a11b76374b3f60aa
shopping <xuzp@linkstec.com>
2019-03-29 22:24:10 +0800

    日程详情

--- TimeApp/src/pages/tddi/tddi.ts
+++ TimeApp/src/pages/tddi/tddi.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   ActionSheetController,  IonicPage, ModalController, NavController, NavParams,
 } from 'ionic-angular';
