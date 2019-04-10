2f705f537caab136ec70e9a8ca323a60bd1ded26
zhangjy <jy-zhang@zakj.info>
2019-04-10 19:50:16 +0800

    Merge branch 'develop' of github.com:XJ-GTD/GTD2 into develop
    # Conflicts:
    #	TimeApp/src/pages/tdl/tdl.ts

--- TimeApp/src/pages/gl/gl.service.ts
+++ TimeApp/src/pages/gl/gl.service.ts
@@ -4,6 +4,7 @@ import {SqliteExec} from "../../service/util-service/sqlite.exec";
 import {GTbl} from "../../service/sqlite/tbl/g.tbl";
 import {BxTbl} from "../../service/sqlite/tbl/bx.tbl";
 import {FsData} from "../../service/pagecom/pgbusi.service";
+import {UserConfig} from "../../service/config/user.config";
 
 @Injectable()
 export class GlService {
@@ -30,23 +31,13 @@ export class GlService {
   }
 
   //获取本地群列表
-  async getGroupl(name:string) {
-    let gld = new PageGlData();
-    //获取本地群列表
-    let sql = 'select * from gtd_g where gn like "'+name+'%"';
-    let dcl: Array<PageDcData> = await this.sqlExce.getExtList<PageDcData>(sql);
-    if(dcl.length>0){
-      //和单群人数
-      for(let dc of dcl){
-        let bx = new BxTbl();
-        bx.bi = dc.gi;
-        let fsl:Array<any> = await this.sqlExce.getList<FsData>(bx);
-        dc.gc = fsl.length;
-        dc.fsl = fsl;
-      }
-      gld.gl = dcl;
-    }
-    return gld;
+  getGroupl(name:string) {
+    if (name)
+    return UserConfig.groups.filter((value)=>{
+      return value.gn.indexOf(name) > -1 || value.gnpy.indexOf(name) > -1
+    });
+    else
+      return UserConfig.groups;
   }
 }
 
