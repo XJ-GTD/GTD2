37dfd5c416f9bf62e80b536430ec8f5aff1987b6
shopping <xuzp@linkstec.com>
2019-04-11 15:17:33 +0800

    系统日程详情画面显示

--- TimeApp/src/pages/tdc/tdds.ts
+++ TimeApp/src/pages/tdc/tdds.ts
@@ -24,7 +24,11 @@ import {PgBusiService, ScdData} from "../../service/pagecom/pgbusi.service";
       <ion-grid>
         <ion-row right>
           <div class="h-auto " >
-            
+            <ion-buttons start>
+              <button ion-button icon-only (click)="cancel()" class="backbtn-set">
+                <img src="../../assets/imgs/fh2.png"/>
+              </button>
+            </ion-buttons>
           </div>
         </ion-row>
       </ion-grid>
@@ -38,52 +42,22 @@ import {PgBusiService, ScdData} from "../../service/pagecom/pgbusi.service";
         </div>
       </ion-row>
       <ion-row >
-        <div >
-            <button  (click)="toPlanChoose()" ion-button  round class ="btn-jh">{{scd.p.jn==""?"添加计划":scd.p.jn}}</button>
+        <div  class ="lbl-jh">
+          <ion-label class="lbl-jh2" >{{scd.p.jn=="" || scd.p.jn==null?"添加计划":scd.p.jn}}</ion-label>
         </div>
       </ion-row>
       <ion-row >
-          <div class="dt-set">
+          <div class="dtshow-set">
             <ion-item>
                 <ion-label >{{scd.sd | formatedate : "CYYYY/M/DD"}}</ion-label>
             </ion-item>
           </div>
-          <div class="week-set">
+          <div class="weekshow-set">
             <ion-label >{{scd.sd | formatedate : "CWEEK" }}</ion-label>
           </div>
-          <div class="tm-set">
+          <div class="tmshow-set">
             <ion-label >{{alldshow}}</ion-label>
           </div>
-      </ion-row>
-      <ion-row >
-        <div class ="reptlbl repttop"><ion-label>重复</ion-label></div>
-        <div class ="repttop1"><ion-label>{{reptshow}}</ion-label></div>
-      </ion-row>
-      <ion-row >
-        <div class ="reptlbl txtop"><ion-label>提醒</ion-label></div>
-        <div class ="txtop1"><button ion-button  round clear class ="sel-btn-set"
-                     [ngClass]="wake.close == 1?'sel-btn-seled':'sel-btn-unsel'"
-                     (click)="clickwake('0')">关</button></div>
-        <div class ="txtop1"><button ion-button  round clear  class ="sel-btn-set"
-                     [ngClass]="wake.tenm == 1?'sel-btn-seled':'sel-btn-unsel'"
-                     (click)="clickwake('1')">10分钟</button></div>
-        <div class ="txtop1"><button ion-button  round clear  class ="sel-btn-set"
-                     [ngClass]="wake.thirm == 1?'sel-btn-seled':'sel-btn-unsel'"
-                     (click)="clickwake('2')">30分钟</button></div>
-        <div class ="txtop1"><button ion-button  round clear  class ="sel-btn-set"
-                     [ngClass]="wake.oh == 1?'sel-btn-seled':'sel-btn-unsel'"
-                     (click)="clickwake('3')">1小时</button></div>
-        <div class ="txtop1"><button ion-button  round clear  class ="sel-btn-set"
-                     [ngClass]="wake.foh == 1?'sel-btn-seled':'sel-btn-unsel'"
-                     (click)="clickwake('4')">4小时</button></div>
-        <div class ="txtop1"><button ion-button  round clear  class ="sel-btn-set"
-                     [ngClass]="wake.od == 1?'sel-btn-seled':'sel-btn-unsel'"
-                     (click)="clickwake('5')">1天</button></div>
-      </ion-row>
-      <ion-row >
-        <div class = "memo-set">
-          <ion-input type="text" placeholder="备注" [(ngModel)]="scd.bz"></ion-input>
-        </div>
       </ion-row>
     </ion-grid>
   </ion-content>
@@ -97,8 +71,6 @@ export class TddsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private util:UtilService,private  tddiServ : TdcService,
-              public actionSheetCtrl: ActionSheetController,
-              public modalCtrl: ModalController,private  busiServ : PgBusiService
   ) {
 
   }
@@ -107,7 +79,6 @@ export class TddsPage {
   scd :ScdData = new ScdData();
   b:boolean = true;
 
-  reptshow:string ="";
 
 
 
@@ -122,27 +93,6 @@ export class TddsPage {
     this.tddiServ.get(this.navParams.get("si")).then(data=>{
       let bs : BsModel<ScdData> = data;
       Object.assign(this.scd,bs.data);
-
-
-      switch (this.scd.rt){
-        case "0":
-          this.reptshow="关";
-          break;
-        case "1":
-          this.reptshow="每日";
-          break;
-        case "2":
-          this.reptshow="每周";
-          break;
-        case "3":
-          this.reptshow="每月";
-          break;
-        case "4":
-          this.reptshow="每年";
-          break;
-        default:
-          this.reptshow="关";
-      }
 
       this.scd.sd = moment(this.scd.sd).format("YYYY-MM-DD");
       this.scd.st = moment().format("HH:mm");
