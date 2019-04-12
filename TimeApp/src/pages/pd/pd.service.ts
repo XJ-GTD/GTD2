60025ae8b7584c8452c738d40a4c7b4f8700ca02
PenyB <1014113078@qq.com>
2019-04-12 15:26:45 +0800

    计划、备份

--- TimeApp/src/pages/pd/pd.service.ts
+++ TimeApp/src/pages/pd/pd.service.ts
@@ -3,45 +3,41 @@ import {SqliteExec} from "../../service/util-service/sqlite.exec";
 import {ShaeRestful, ShareData} from "../../service/restful/shaesev";
 import {JhTbl} from "../../service/sqlite/tbl/jh.tbl";
 import {CTbl} from "../../service/sqlite/tbl/c.tbl";
-import {ETbl} from "../../service/sqlite/tbl/e.tbl";
-import {DTbl} from "../../service/sqlite/tbl/d.tbl";
 import {BsModel} from "../../service/restful/out/bs.model";
 import {AgdPro} from "../../service/restful/agdsev";
 
 @Injectable()
 export class PdService {
-  constructor(private sqlExce: SqliteExec,
-              private shareRestful:ShaeRestful,
-  ) {
-  }
+  constructor(private sqlExec: SqliteExec,
+              private shareRestful: ShaeRestful,) {}
 
   //获取计划 计划详情
-  async getPlan(pid:string){
+  async getPlan(pid: string) {
     console.log('---------- PdService getPlan 获取计划开始 ----------------');
     //获取本地计划
     let jh: JhTbl = new JhTbl();
     jh.ji = pid;
-    jh = await this.sqlExce.getOne<JhTbl>(jh);
+    jh = await this.sqlExec.getOne<JhTbl>(jh);
 
     // 获取计划管理日程（重复日程处理等）
-    let sql = 'select * from gtd_c  where ji ="'+ pid +'" order by sd';
+    let sql = 'select * from gtd_c  where ji ="' + pid + '" order by sd';
 
     sql = 'select gc.si,gc.sn,gc.bz,gc.ji,gc.rt,gc.sr,sp.sd,sp.st,sp.et,sp.ed from gtd_c gc ' +
       'left join gtd_sp sp on sp.si = gc.si ' +
-      'where gc.ji = "'+ pid + '" order by sp.sd,sp.st desc';
-    let cs = new  Array<CTbl>();
-    cs = await this.sqlExce.getExtList<CTbl>(sql);
+      'where gc.ji = "' + pid + '" order by sp.sd,sp.st desc';
+    let cs = new Array<CTbl>();
+    cs = await this.sqlExec.getExtList<CTbl>(sql);
 
-    let paList:Array<AgdPro> = Array<AgdPro>();
-    if(cs.length>0){
+    let paList: Array<AgdPro> = Array<AgdPro>();
+    if (cs.length > 0) {
       console.log('---------- PdService getPlan 获取计划日程开始 ----------------');
       //获取计划日程
-      for(let jhc of cs){
-        let pa:AgdPro = new AgdPro();
+      for (let jhc of cs) {
+        let pa: AgdPro = new AgdPro();
 
         pa.ai = jhc.si;//日程ID
         pa.at = jhc.sn;//主题
-        pa.adt = jhc.sd +" "+ jhc.st ;//时间(YYYY/MM/DD HH:mm)
+        pa.adt = jhc.sd + " " + jhc.st;//时间(YYYY/MM/DD HH:mm)
         pa.st = jhc.st;
         pa.et = jhc.et;//结束日期
         pa.ed = jhc.ed;//结束时间
@@ -63,85 +59,85 @@ export class PdService {
   }
 
   //分享计划
-   sharePlan(plan:any):Promise<any>{
+  sharePlan(plan: any): Promise<any> {
 
-     return new Promise((resolve, reject) => {
-       console.log('---------- PdService sharePlan 分享计划开始 ----------------');
+    return new Promise((resolve, reject) => {
+      console.log('---------- PdService sharePlan 分享计划开始 ----------------');
 
-       let shareData:ShareData = new ShareData();
-       shareData.ompn="";
-       shareData.oai="";
-       shareData.d.p.pn = plan.pn;
-       shareData.d.p.pa = plan.pa;
+      let shareData: ShareData = new ShareData();
+      shareData.ompn = "";
+      shareData.oai = "";
+      shareData.d.p.pn = plan.pn;
+      shareData.d.p.pa = plan.pa;
 
-       //restful上传计划
-       this.shareRestful.share(shareData).then(data=>{
-         console.log('---------- PdService sharePlan 分享计划结束 ----------------');
-         // 返出参
-         resolve(data);
-       })
+      //restful上传计划
+      this.shareRestful.share(shareData).then(data => {
+        console.log('---------- PdService sharePlan 分享计划结束 ----------------');
+        // 返出参
+        resolve(data);
+      })
 
-     })
+    })
 
   }
 
-  //删除计划
-  async delete(pid:string):Promise<BsModel<any>>{
+  //删除系统计划
+  async delete(jh: PagePDPro): Promise<BsModel<any>> {
     console.log('---------- PdService delete 删除计划开始 ----------------');
-    //获取本地计划
-    let jhTbl: JhTbl = new JhTbl();
-    jhTbl.ji = pid;
-    jhTbl = await this.sqlExce.getOne<JhTbl>(jhTbl);
+    if (jh.jt == "2") {
+      //获取本地计划
+      let jhTbl: JhTbl = new JhTbl();
+      jhTbl.ji = jh.ji;
+      jhTbl = await this.sqlExec.getOne<JhTbl>(jhTbl);
 
-    // 删除本地计划日程关联
-    //获取计划管理日程
-    let ctbl:CTbl =new CTbl();
-    ctbl.ji = jhTbl.ji;
+      // 本地计划日程关联
+      //修改日程表 计划为 null
+      let sql = 'update gtd_c set ji= null where ji = "' + jhTbl.ji + '";';
+      this.sqlExec.execSql(sql);
 
-    //修改日程表 计划为 null
-    let sql ='update gtd_c set ji= null where ji = "'+ jhTbl.ji +'";';
-    this.sqlExce.execSql(sql);
+      //修改日程表 计划为 null
+      let spSql = 'update gtd_sp set ji= null where ji = "' + jhTbl.ji + '";';
+      this.sqlExec.execSql(spSql);
 
-    //修改日程表 计划为 null
-    let spSql ='update gtd_sp set ji= null where ji = "'+ jhTbl.ji +'";';
-    this.sqlExce.execSql(spSql);
+      /*
+      //获取计划管理日程
+      let ctbl: CTbl = new CTbl();
+      ctbl.ji = jhTbl.ji;
+      let ctbls = await this.sqlExec.getList<CTbl>(ctbl);
 
-    /*let ctbls = await this.sqlExce.getList<CTbl>(ctbl);
+      if(ctbls.length > 0){
+        for (let j = 0, len = ctbls.length; j < len; j++) {
+          //提醒删除
+          let etbl:ETbl =new ETbl();
+          etbl.si = ctbls[j].si;
+          await this.sqlExec.delete(etbl);
 
-    if(ctbls.length > 0){
-      for (let j = 0, len = ctbls.length; j < len; j++) {
-        //提醒删除
-        let etbl:ETbl =new ETbl();
-        etbl.si = ctbls[j].si;
-        await this.sqlExce.delete(etbl);
+          //日程参与人删除
+          let dtbl:DTbl =new DTbl();
+          dtbl.si = ctbls[j].si;
+          await this.sqlExec.delete(dtbl);
+        }
+        //计划关联日程删除
+        await this.sqlExec.delete(ctbl);
 
-        //日程参与人删除
-        let dtbl:DTbl =new DTbl();
-        dtbl.si = ctbls[j].si;
-        await this.sqlExce.delete(dtbl);
-      }
-      //计划关联日程删除
-      await this.sqlExce.delete(ctbl);
+      }else{
+        console.log('---------- PdService delete 计划管理日程无数据 ----------------');
+      }*/
 
-    }else{
-      console.log('---------- PdService delete 计划管理日程无数据 ----------------');
-    }*/
+      // 删除本地计划
+      let tbl: JhTbl = new JhTbl();
+      tbl.ji = jhTbl.ji;
+      await this.sqlExec.delete(tbl);
 
-    // 删除本地计划
-    let tbl: JhTbl = new JhTbl();
-    tbl.ji = jhTbl.ji;
-    await this.sqlExce.delete(tbl);
+      // TODO restful删除分享计划
+      console.log('---------- PdService delete 删除计划结束 ----------------');
 
-
-
-    // TODO restful删除分享计划
-    console.log('---------- PdService delete 删除计划结束 ----------------');
-    // 返出参
-    let bs = new BsModel();
-    bs.code = 0;
-    return bs;
+      // 返出参
+      let bs = new BsModel();
+      bs.code = 0;
+      return bs;
+    }
   }
-
 }
 
 //页面项目
