4a8e6001b2d5fd3ac4659610a59f2703d1a46dbb
zhangjy <jy-zhang@zakj.info>
2019-04-12 20:56:01 +0800

    修正

--- TimeApp/src/pages/al/al.service.ts
+++ TimeApp/src/pages/al/al.service.ts
@@ -115,7 +115,7 @@ export class AlService {
       yTbl.yv = "0";
       await this.sqlExce.replaceT(yTbl);
 
-      await this.createTestData();
+      //await this.createTestData();
 
       await this.contactsService.asyncPhoneContacts();
 
