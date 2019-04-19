e2e19e78e385dcf64ca1e4e948692351890120f6
leon_xi <leon_xi@163.com>
2019-04-19 20:24:57 +0800

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -14,7 +14,7 @@ cp -rf $TRAVIS_BUILD_DIR/TimeAppPatch/platforms/* $TRAVIS_BUILD_DIR/TimeApp/plat
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   ionic cordova build ios
-  xctool -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME-dev -sdk iphoneos -configuration Debug CODE_SIGN_RESOURCE_RULES_PATH='$(SDKROOT)/ResourceRules.plist' OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
+  xctool -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME -sdk iphoneos -configuration Debug CODE_SIGN_RESOURCE_RULES_PATH='$(SDKROOT)/ResourceRules.plist' OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
 else
   ionic cordova build android
 fi
