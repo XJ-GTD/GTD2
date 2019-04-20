ebd654285fa7c485a068e6e526e621bb62ba5ab5
leon_xi <leon_xi@163.com>
2019-04-20 10:41:28 +0800

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-@echo on
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # Build on macOS
@@ -17,9 +16,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   ionic cordova build ios
   cd $TRAVIS_BUILD_DIR/TimeApp/platforms/ios
   # CODE_SIGN_RESOURCE_RULES_PATH='$(PROJECT_DIR)/$(PROJECT_NAME)/Entitlements-$(CONFIGURATION).plist' OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
-  xctool -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME -sdk iphoneos -configuration Debug build
+  xctool -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME -sdk iPhoneOS12.2.sdk -configuration Debug build
 else
   ionic cordova build android
 fi
-
-@echo off
