d805a7025273428b067234a9468fa0841a9464e5
Leon <leon_xi@163.com>
2019-04-23 01:27:42 +0800

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -18,7 +18,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   ionic cordova build ios
   cd $TRAVIS_BUILD_DIR/TimeApp/platforms/ios
   # CODE_SIGN_RESOURCE_RULES_PATH='$(PROJECT_DIR)/$(PROJECT_NAME)/Entitlements-$(CONFIGURATION).plist' OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
-  xcodebuild build -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME -sdk iphoneos -configuration Debug CODE_SIGN_RESOURCE_RULES_PATH='$(PROJECT_DIR)/$(PROJECT_NAME)/Entitlements-$(CONFIGURATION).plist' CODE_SIGN_IDENTITY="$IOS_DEVELOPER_NAME" PROVISIONING_PROFILE="FAFF23B4FAEF7383978A664364DC47F8F7D8238B" ONLY_ACTIVE_ARCH=NO
+  xcodebuild build -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME -sdk iphoneos -configuration Debug CODE_SIGN_RESOURCE_RULES_PATH='$(PROJECT_DIR)/$(PROJECT_NAME)/Entitlements-$(CONFIGURATION).plist' CODE_SIGN_IDENTITY="$IOS_DEVELOPER_NAME" PROVISIONING_PROFILE="$IOS_PROFILE_NAME" ONLY_ACTIVE_ARCH=NO
 else
   ionic cordova build android
 fi
