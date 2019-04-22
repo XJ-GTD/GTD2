05828b58f9c73381e352febca5570b1f14745e57
Leon <leon_xi@163.com>
2019-04-23 00:52:09 +0800

    Update add-keys.sh

--- travis/add-keys.sh
+++ travis/add-keys.sh
@@ -12,10 +12,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   # Add certificates to keychain and allow codesign to access them
   security import $TRAVIS_BUILD_DIR/travis/profiles/ios/mwxing.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
   # 正式发布证书
-  #security import $TRAVIS_BUILD_DIRtravis/profiles/ios/mwxing.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+  #security import $TRAVIS_BUILD_DIR/travis/profiles/ios/mwxing.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
   security import $TRAVIS_BUILD_DIR/travis/profiles/ios/mwxing-developer.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
   # 正式发布证书
-  security import $TRAVIS_BUILD_DIRtravis/profiles/ios/mwxing.p12 -k ~/Library/Keychains/ios-build.keychain -P $IOS_KEY_PASSWORD -T /usr/bin/codesign
+  security import $TRAVIS_BUILD_DIR/travis/profiles/ios/mwxing.p12 -k ~/Library/Keychains/ios-build.keychain -P $IOS_KEY_PASSWORD -T /usr/bin/codesign
   security import $TRAVIS_BUILD_DIR/travis/profiles/ios/mwxing-developer.p12 -k ~/Library/Keychains/ios-build.keychain -P $IOS_KEY_PASSWORD -T /usr/bin/codesign
    
   echo "list keychains: "
