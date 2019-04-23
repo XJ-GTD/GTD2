79ea3a2dc8a0b94e4b95823b543be0e0d5231cbd
Leon <leon_xi@163.com>
2019-04-23 22:38:53 +0800

    Update preinstall.sh

--- travis/preinstall.sh
+++ travis/preinstall.sh
@@ -2,6 +2,7 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   gem install xcpretty
+  brew install yarn
   brew install git-lfs
   git clone --depth=50 --branch=master https://github.com/leonxi/largefiles.git $HOME/build/leonxi/largefiles
   pwd
