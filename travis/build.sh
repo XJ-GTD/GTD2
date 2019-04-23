db07aaab40c6c5932428e300b4401b856012333e
Leon <leon_xi@163.com>
2019-04-23 08:54:04 +0800

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -15,7 +15,8 @@ fi
 cp -rf $TRAVIS_BUILD_DIR/TimeAppPatch/platforms/* $TRAVIS_BUILD_DIR/TimeApp/platforms
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  ionic cordova build ios --buildConfig $TRAVIS_BUILD_DIR/travis/profiles/cordova/build.json
+  #ionic cordova build ios --buildConfig $TRAVIS_BUILD_DIR/travis/profiles/cordova/build.json
+  ionic cordova build ios
   cd $TRAVIS_BUILD_DIR/TimeApp/platforms/ios
   # CODE_SIGN_RESOURCE_RULES_PATH='$(PROJECT_DIR)/$(PROJECT_NAME)/Entitlements-$(CONFIGURATION).plist' OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
   xcodebuild build -workspace $IOS_APP_NAME.xcworkspace -scheme $IOS_APP_NAME -sdk iphoneos -configuration Debug CODE_SIGN_RESOURCE_RULES_PATH='$(PROJECT_DIR)/$(PROJECT_NAME)/Entitlements-$(CONFIGURATION).plist' CODE_SIGN_IDENTITY="$IOS_DEVELOPER_NAME" PROVISIONING_PROFILE="$IOS_PROFILE_NAME" ONLY_ACTIVE_ARCH=NO
