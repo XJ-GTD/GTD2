62910cf934ec7d241c021c3c7842f57e22f874e9
leon_xi <leon_xi@163.com>
2019-05-01 13:49:31 +0800

    更新ipa文件名

--- travis/fir.sh
+++ travis/fir.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # fir on macOS
     #fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/ios/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
     ls $TRAVIS_BUILD_DIR/build/debug
-    fir p $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa -T $FIR_TOKEN
+    fir p $TRAVIS_BUILD_DIR/build/debug/mwxing-debug.ipa -T $FIR_TOKEN
 else
     # fir on Linux
     fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
