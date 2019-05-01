de60e3d07639bd759dcdf90820b83cead33ce113
leon_xi <leon_xi@163.com>
2019-05-01 20:27:24 +0800

    更新iOS打包

--- travis/fir.sh
+++ travis/fir.sh
@@ -3,8 +3,8 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # fir on macOS
     #fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/ios/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
-    ls -la $TRAVIS_BUILD_DIR/build/debug/mwxing-debug.ipa
-    fir publish $TRAVIS_BUILD_DIR/build/debug/mwxing-debug.ipa -T $FIR_TOKEN
+    ls -la $TRAVIS_BUILD_DIR/build/debug
+    fir publish $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa -T $FIR_TOKEN
 else
     # fir on Linux
     fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
