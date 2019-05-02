2b03c7f0fc2290a6b303c330a302ebbda27d09bc
leon_xi <leon_xi@163.com>
2019-05-02 21:27:57 +0800

    设置访问冥王星生产环境

--- TimeApp/src/service/restful/syncsev.ts
+++ TimeApp/src/service/restful/syncsev.ts
@@ -9,7 +9,7 @@ import {UtilService} from "../util-service/util.service";
  */
 @Injectable()
 export class SyncRestful {
-  private initDataUrl: string = "https://www.guobaa.com/ini/parameters?debug=false";
+  private initDataUrl: string = "https://www.guobaa.com/ini/parameters?tag=mwxing";
 
   constructor(private request: RestfulClient, private uitl: UtilService) {
   }
