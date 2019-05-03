0e5d906b27e3394926952ef4f6f20d9f6644c4b4
leon_xi <leon_xi@163.com>
2019-05-03 15:57:28 +0800

    增加预览版自动发布脚本

--- travis/fir.sh
+++ travis/fir.sh
@@ -8,7 +8,11 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     cat $TRAVIS_BUILD_DIR/build/debug/ExportOptions.plist
     cat $TRAVIS_BUILD_DIR/build/debug/Packaging.log
     fir publish $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa -T $FIR_TOKEN
+    cp $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa
+    fir publish $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa -T $FIR_TOKEN
 else
     # fir on Linux
     fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
+    cp $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk
+    fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk -T $FIR_TOKEN
 fi
