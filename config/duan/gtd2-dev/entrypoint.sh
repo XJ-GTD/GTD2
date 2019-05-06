7c2e1ea177aa22ffd86442f46bf518002bf24a70
leon_xi <leon_xi@163.com>
2019-05-06 20:24:51 +0800

    更新部署文件

--- config/duan/gtd2-dev/entrypoint.sh
+++ config/duan/gtd2-dev/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+cat /etc/hosts
+
 cd /opt/gtd2
 git pull
 
-cd /opt/gtd2/TimeApp
-ionic cordova run browser -lcs --address 0.0.0.0
+#cd /opt/gtd2/TimeApp
+#ionic cordova run browser -lcs --address=10.0.0.84
 
