e01ce7a695baf965004aa98ae7b57b99debcfb5f
leon_xi <leon_xi@163.com>
2019-05-07 14:50:54 +0800

    Update find.process.ts

--- TimeApp/src/ws/process/find.process.ts
+++ TimeApp/src/ws/process/find.process.ts
@@ -6,11 +6,11 @@ import {F} from "../model/ws.enum";
 import {FindPara} from "../model/find.para";
 import {SqliteExec} from "../../service/util-service/sqlite.exec";
 import * as moment from "moment";
-import {StringSimilarity} from 'string-similarity';
 import {CTbl} from "../../service/sqlite/tbl/c.tbl";
 import {FsService} from "../../pages/fs/fs.service";
 import {GlService} from "../../pages/gl/gl.service";
 import {FsData, PageDcData} from "../../data.mapping";
+import {UtilService} from "../../service/util-service/util.service";
 
 /**
  * 查询联系人和日历
@@ -19,7 +19,7 @@ import {FsData, PageDcData} from "../../data.mapping";
  */
 @Injectable()
 export class FindProcess implements MQProcess {
-  constructor(private sqliteExec: SqliteExec, private fsService: FsService, private glService: GlService) {
+  constructor(private sqliteExec: SqliteExec, private fsService: FsService, private glService: GlService, private util:UtilService) {
   }
 
   async go(content: WsContent, processRs: ProcesRs) {
@@ -101,7 +101,7 @@ export class FindProcess implements MQProcess {
       let piny = n.n;
       //首先查找群组
       for (let g of gs) {
-        if (StringSimilarity.compareTwoStrings(piny, b3.ranpy) > 0.8) {
+        if (this.util.compareTwoStrings(piny, b3.ranpy) > 0.8) {
           //piny = piny.replace(g.gnpy, "");
           for (let b1 of g.fsl) {
             rsbs.set(b1.ranpy, b1);
@@ -116,7 +116,7 @@ export class FindProcess implements MQProcess {
       let piny = n.n;
       //首先查找群组
       for (let b3 of bs) {
-        if (StringSimilarity.compareTwoStrings(piny, g.gnpy) > 0.8) {
+        if (this.util.compareTwoStrings(piny, g.gnpy) > 0.8) {
           //piny = piny.replace(b3.ranpy, "").replace(b3.rnpy, "");
           rsbs.set(b3.ranpy, b3);
         }
