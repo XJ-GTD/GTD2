2a7cea37ccd3dd7bb571cc4217d96f69298a143c
leon_xi <leon_xi@163.com>
2019-05-22 17:54:59 +0800

    更新消息协议，禁止修改时播报日程所属人

--- config/duan/acj/v2/modify_agenda.js
+++ config/duan/acj/v2/modify_agenda.js
@@ -455,7 +455,11 @@ function clean(datasource)
         t: 'EU'
       },
       input: {
-        type: 'function(agendas, showagendas, prvOpt, user, branchtype, branchcode) { return branchcode; }'
+        type: 'function(agendas, showagendas, prvOpt, user, branchtype, branchcode) { return branchcode; }',
+        textvariables: [
+          {name: 'agendaowner', expression: 'agendas[0].uinm', default: '他人'}
+        ],
+        showagendas: ""
       }
     };
   } else {
