7cece09fca0a3c39c18f797df0b354eb4f880c81
leon_xi <leon_xi@163.com>
2019-06-03 17:39:49 +0800

    更新

--- TimeApp/src/pages/dr/dr.module.ts
+++ TimeApp/src/pages/dr/dr.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import {DrService} from "../dr/dr.service";
 import {DrPage} from "./dr";
+import {PipesModule} from "../../pipes/pipes.module";
 
 @NgModule({
   declarations: [
     DrPage,
   ],
   imports: [
+    PipesModule,
     IonicPageModule.forChild(DrPage),
   ],
   providers: [
