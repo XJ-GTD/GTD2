82f5b0dc710b74ffd7b24528f3e37c62132f1561
leon_xi <leon_xi@163.com>
2019-06-05 15:50:41 +0800

    更新

--- TimeApp/src/pages/da/da.service.ts
+++ TimeApp/src/pages/da/da.service.ts
@@ -28,7 +28,7 @@ export class DaService {
   }
 
   speakDailySummary(currentday: moment.Moment, scdlist: Array<ScdData>) {
-    let currentdaystring = currentday.format("yyyy/MM/dd");
+    let currentdaystring = currentday.format("YYYY/MM/DD");
     let speak = currentday.format("M月d日");
     speak = speak + " " + currentday.format('dddd') + "。";
 
