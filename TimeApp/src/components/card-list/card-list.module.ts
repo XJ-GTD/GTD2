950280aae92ced8480187e065f27c0518819f9e9
leon_xi <leon_xi@163.com>
2019-06-07 17:47:28 +0800

    更新

--- TimeApp/src/components/card-list/card-list.module.ts
+++ TimeApp/src/components/card-list/card-list.module.ts
@@ -1,4 +1,4 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CardListComponent } from './card-list';
 
