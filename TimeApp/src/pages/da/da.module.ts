0fbd08a780eab567107efb2e928e063b38bc5b62
leon_xi <leon_xi@163.com>
2019-06-07 17:56:32 +0800

    更新

--- TimeApp/src/pages/da/da.module.ts
+++ TimeApp/src/pages/da/da.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DaPage } from "./da";
 import { DaService } from "./da.service";
+import {CardListComponentModule} from "../../components/card-list/card-list.module";
 
 @NgModule({
   declarations: [
