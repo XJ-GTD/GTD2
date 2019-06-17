5533588d00eed8c498335a185c052033b942e61d
leon_xi <leon_xi@163.com>
2019-06-17 23:43:52 +0800

    更新

---
+++ TimeApp/src/config/copy.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  copyCropperjs: {
+    src: [
+      "{{ROOT}}/node_modules/weather-icons/css/weather-icons.css",
+      "{{ROOT}}/node_modules/weather-icons/css/weather-icons-wind.css"
+    ],
+    dest: "{{BUILD}}/assets/"
+  }
+}
