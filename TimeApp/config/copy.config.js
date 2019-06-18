390b32068e754fb004ee7ae44a94a731aad10e28
leon_xi <leon_xi@163.com>
2019-06-18 10:58:13 +0800

    增加打包配置

---
+++ TimeApp/config/copy.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  copyFonts: {
+    src: ['{{ROOT}}/node_modules/weather-icons/fonts/*'],
+    dest: '{{WWW}}/assets/fonts'
+  },
+  copyFonts: {
+    src: ['{{ROOT}}/node_modules/weather-icons/css/*'],
+    dest: '{{WWW}}/assets/css'
+  }
+}
--- TimeApp/src/config/copy.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  copyCropperjs: {
-    src: [
-      "{{ROOT}}/node_modules/weather-icons/css/weather-icons.css",
-      "{{ROOT}}/node_modules/weather-icons/css/weather-icons-wind.css"
-    ],
-    dest: "{{BUILD}}/assets/"
-  }
-}
