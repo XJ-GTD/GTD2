2e211ab41ede75795a0632568ed5c0edacb43628
leon_xi <leon_xi@163.com>
2019-06-26 21:52:29 +0800

    更新引用位置

--- TimeApp/src/pages/tdm/tdm.module.ts
+++ TimeApp/src/pages/tdm/tdm.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { TdmPage } from './tdm';
 import {IonicModule} from "ionic-angular";
 import {PipesModule} from "../../pipes/pipes.module";
-import {ScrollSelectComponentModule} from "../components/scroll-select/scroll-select.module";
+import {ScrollSelectComponentModule} from "../../components/scroll-select/scroll-select.module";
 
 @NgModule({
   declarations: [
