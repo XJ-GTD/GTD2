c928b4a7d4a98867ff3a1531f181db1f2d22bacc
leon_xi <leon_xi@163.com>
2019-06-26 22:01:14 +0800

    移除不需要的日历组件

--- TimeApp/src/components/ion2-calendar/components/index.ts
+++ TimeApp/src/components/ion2-calendar/components/index.ts
@@ -2,12 +2,10 @@ import { CalendarWeekComponent } from "./calendar-week.component";
 import { MonthComponent } from "./month.component";
 import { CalendarComponent } from "./calendar.component";
 import { MonthPickerComponent } from "./month-picker.component";
-import {ScrollSelectComponent} from "../../scroll-select/scroll-select";
 
 export const CALENDAR_COMPONENTS = [
   CalendarWeekComponent,
   MonthComponent,
   CalendarComponent,
-  MonthPickerComponent,
-  ScrollSelectComponent
+  MonthPickerComponent
 ];
