cf19d47f3ee5b7cc6938a7f1427387ae98d4d1eb
leon_xi <leon_xi@163.com>
2019-06-28 19:11:38 +0800

    更新

--- TimeApp/src/pages/tdm/tdm.module.ts
+++ TimeApp/src/pages/tdm/tdm.module.ts
@@ -6,6 +6,7 @@ import {PipesModule} from "../../pipes/pipes.module";
 import {ScrollSelectComponentModule} from "../../components/scroll-select/scroll-select.module";
 import {RadioSelectComponentModule} from "../../components/radio-select/radio-select.module";
 import {ScrollRangePickerComponentModule} from "../../components/scroll-range-picker/scroll-range-picker.module";
+import { DirectivesModule } from "../../directives/directives.module";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {ScrollRangePickerComponentModule} from "../../components/scroll-range-pi
   imports: [
       IonicModule,
       PipesModule,
+      DirectivesModule,
       ScrollSelectComponentModule,
       RadioSelectComponentModule,
       ScrollRangePickerComponentModule
