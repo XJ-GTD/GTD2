967de337c5d64a7898b7f7a63a21cb4df296a523
leon_xi <leon_xi@163.com>
2019-06-29 08:54:21 +0800

    更新

--- TimeApp/src/components/scroll-range-picker/scroll-range-picker.ts
+++ TimeApp/src/components/scroll-range-picker/scroll-range-picker.ts
@@ -88,6 +88,7 @@ export class ScrollRangePickerComponent {
 
     let pixels = document.body.clientWidth;
     let canView = pixels / 5 * this.viewMinTime;
+    this.viewBoxPointer = '0 0 ' + canView + ' 180';
 
     // 画范围外时间线 (包括范围之前和范围之后)
     for (let hour = 0; hour < this.viewHours; hour++) {
