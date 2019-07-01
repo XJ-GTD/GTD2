5b01e1de8cf45681994eaf14c0dbb4d317d2b8d8
leon_xi <leon_xi@163.com>
2019-07-01 17:40:16 +0800

    更新

--- TimeApp/src/components/speech-bubble/speech-bubble.ts
+++ TimeApp/src/components/speech-bubble/speech-bubble.ts
@@ -11,7 +11,7 @@ import * as moment from "moment";
 @Component({
   selector: 'speech-bubble',
   template: `
-  <div class="outtitle">{{recordat| formatedate:"CYYYY年MM月DD日 A h:mm"}}</div>
+  <div class="outtitle" *ngIf="!samewithprev">{{recordat| formatedate:"CYYYY年MM月DD日 A h:mm"}}</div>
   <div class="outground" align-items-center *ngIf="backgroundColor" [ngStyle]="{'background-color': backgroundColor }">
     <ion-icon name="radio"></ion-icon> <span>{{seconds}}"</span>
   </div>
@@ -28,6 +28,9 @@ export class SpeechBubbleComponent {
   seconds: number = 1;
   @Input("at")
   recordat: number = moment().valueOf();
+  @Input("prevat")
+  prevrecordat: number = moment().valueOf();
+  samewithprev: boolean = false;
 
   constructor(public events: Events) {
     console.log('Hello SpeechBubbleComponent Component');
