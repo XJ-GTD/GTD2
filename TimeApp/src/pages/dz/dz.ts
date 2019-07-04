1444b60c36bd7d94a696e958cdb2b565121eb6dc
leon_xi <leon_xi@163.com>
2019-07-04 21:40:14 +0800

    更新

--- TimeApp/src/pages/dz/dz.ts
+++ TimeApp/src/pages/dz/dz.ts
@@ -1,13 +1,15 @@
 import { Component, ElementRef, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, Scroll } from 'ionic-angular';
-import { MapOptions } from 'angular2-baidu-map';
+import { MapOptions, NavigationControlOptions, NavigationControlType } from 'angular2-baidu-map';
 
 @IonicPage()
 @Component({
   selector: 'page-dz',
   template: `
   <ion-content>
-    <baidu-map [options]="options"></baidu-map>
+    <baidu-map [options]="options">
+      <control type="navigation" [options]="navOptions"></control>
+    </baidu-map>
     <ion-fab bottom center>
       <button ion-fab (click)="close()">
         <ion-icon name="checkmark"></ion-icon>
@@ -18,6 +20,7 @@ import { MapOptions } from 'angular2-baidu-map';
 })
 export class DzPage {
   options: MapOptions;  //百度地图选项
+  navOptions: NavigationControlOptions; //百度导航条选项
 
   constructor(public navCtrl: NavController) {
 
@@ -29,7 +32,13 @@ export class DzPage {
         zoom: 8
       },
       enableKeyboard: true
-    }
+    };
+
+    //百度地图导航条选项
+    this.navOptions = {
+  		anchor: ControlAnchor.BMAP_ANCHOR_TOP_RIGHT,
+  		type: NavigationControlType.BMAP_NAVIGATION_CONTROL_PAN
+	  };
   }
 
   close() {
