48cfa067b692265e9c44c8d002f5adeef6299282
leon_xi <leon_xi@163.com>
2019-07-05 00:30:54 +0800

    更新

--- travis/fir.sh
+++ travis/fir.sh
@@ -9,7 +9,7 @@ if (( $TRAVIS_OS_NAME == 'osx' )); then
     cat $TRAVIS_BUILD_DIR/build/debug/Packaging.log
     #fir publish $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa -T $FIR_TOKEN
     cp $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa
-    if (( $TRAVIS_BRANCH = 'cassiscornuta' )); then
+    if (( '$TRAVIS_BRANCH' = 'cassiscornuta' )); then
       fir publish $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa -T $FIR_TOKEN_CASSISCORNUTA
     else
       fir publish $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa -T $FIR_TOKEN
@@ -18,7 +18,7 @@ else
     # fir on Linux
     #fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
     cp $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk
-    if (( $TRAVIS_BRANCH = 'cassiscornuta' )); then
+    if (( '$TRAVIS_BRANCH' = 'cassiscornuta' )); then
       fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk -T $FIR_TOKEN_CASSISCORNUTA
     else
       fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk -T $FIR_TOKEN
