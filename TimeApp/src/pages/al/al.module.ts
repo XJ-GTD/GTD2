f6c119c2c7e626e9b12bb5ad76bdf7b1c8236bdf
leon_xi <leon_xi@163.com>
2019-07-04 17:38:54 +0800

    更新

--- TimeApp/src/pages/al/al.module.ts
+++ TimeApp/src/pages/al/al.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {IonicPageModule} from 'ionic-angular';
 import {AlPage} from './al';
-import {RoundProgressModule} from 'angular-svg-round-progressbar';
 import {AlService} from "./al.service";
 import {BackComponentModule} from "../../components/backComponent/back.module";
 import {TdcPageModule} from "../tdc/tdc.module";
@@ -12,7 +11,6 @@ import {TdcPageModule} from "../tdc/tdc.module";
   ],
   imports: [
     IonicPageModule.forChild(AlPage),
-    RoundProgressModule,
     BackComponentModule,
     TdcPageModule,
   ],
