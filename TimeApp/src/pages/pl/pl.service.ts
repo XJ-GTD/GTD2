06ba4542c2117ca0ef05cdb1028ea9d36a3b7d64
leon_xi <leon_xi@163.com>
2019-07-05 22:51:59 +0800

    更新

--- TimeApp/src/pages/pl/pl.service.ts
+++ TimeApp/src/pages/pl/pl.service.ts
@@ -116,7 +116,7 @@ export class PlService {
     return pld;
   }
 
-  async getJh(ji: string, prop: string = 'all') {
+  async getJh(ji: string, prop: string = 'all'): any {
     let jh:JhTbl = new JhTbl();
     jh.ji = ji;
 
