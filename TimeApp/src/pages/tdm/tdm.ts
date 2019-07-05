090d980edc36b2e98239f0f701fb0cda377407cc
leon_xi <leon_xi@163.com>
2019-07-05 22:50:44 +0800

    更新

--- TimeApp/src/pages/tdm/tdm.ts
+++ TimeApp/src/pages/tdm/tdm.ts
@@ -125,7 +125,7 @@ export class TdmPage {
   getDefaultJh() {
     this.util.loadingStart();
     this.plService.getPlan().then(data=>{
-      this.jhoptions = data.zdyJh;
+      let jhoptions = data.zdyJh;
 
       this.util.loadingEnd();
     }).catch(error=>{
