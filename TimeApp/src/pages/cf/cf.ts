442cf32e1a3dca1b840a4f99a42f6af9bd84d017
leon_xi <leon_xi@163.com>
2019-07-06 20:56:24 +0800

    更新

--- TimeApp/src/pages/cf/cf.ts
+++ TimeApp/src/pages/cf/cf.ts
@@ -41,7 +41,7 @@ import { RadioSelectComponent } from "../../components/radio-select/radio-select
             <p>结束</p>
           </ion-row>
           <ion-row justify-content-start>
-            <ion-list class="endwith" radio-group no-lines [(ngModel)]="cfEndType" (ionChange)="onEndTypeChanged($event)">
+            <ion-list class="endwith" radio-group no-lines [(ngModel)]="cfEndType">
               <ion-item>
                 <ion-radio item-start value="never"></ion-radio>
                 <ion-label>永远不</ion-label>
