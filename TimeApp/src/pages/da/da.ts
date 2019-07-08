cfe1d17a20d0a747edefd73b52f26135ef9c0c2c
leon_xi <leon_xi@163.com>
2019-07-08 13:48:54 +0800

    更新

--- TimeApp/src/pages/da/da.ts
+++ TimeApp/src/pages/da/da.ts
@@ -41,17 +41,15 @@ import {CardListComponent} from "../../components/card-list/card-list";
     </ion-header>
 
     <ion-content padding>
-      <ion-scroll>
-        <ion-refresher (ionRefresh)='doRefresh($event)'>
-          <ion-refresher-content pullingIcon="arrow-dropdown" pullingText="下拉添加" refreshingSpinner="circles" refreshingText="添加">   
-          </ion-refresher-content>
-        </ion-refresher>
-        <ion-slides [initialSlide]="INIT_SLIDE_DAY" (ionSlideDidChange)="slideChanged()" (ionSlideNextEnd)="slideNextEnd()" (ionSlidePrevEnd)="slidePrevEnd()">
-          <ion-slide *ngFor="let day of days" class="scroll-y">
-            <card-list #cardlist (onStartLoad)="getData($event, day)" (onCardClick)="gotoDetail($event)" (onCreateNew)="goNew()" #cardlist></card-list>
-          </ion-slide>
-        </ion-slides>
-      </ion-scroll>
+      <ion-refresher (ionRefresh)='doRefresh($event)'>
+        <ion-refresher-content pullingIcon="arrow-dropdown" pullingText="下拉添加" refreshingSpinner="circles" refreshingText="添加">   
+        </ion-refresher-content>
+      </ion-refresher>
+      <ion-slides [initialSlide]="INIT_SLIDE_DAY" (ionSlideDidChange)="slideChanged()" (ionSlideNextEnd)="slideNextEnd()" (ionSlidePrevEnd)="slidePrevEnd()">
+        <ion-slide *ngFor="let day of days" class="scroll-y">
+          <card-list #cardlist (onStartLoad)="getData($event, day)" (onCardClick)="gotoDetail($event)" (onCreateNew)="goNew()" #cardlist></card-list>
+        </ion-slide>
+      </ion-slides>
     </ion-content>
 
     <ion-footer class="foot-set" *ngIf="isMobile && hasContents">
