04404bc506491bf413c71c015a600ae8a84b6bde
leon_xi <leon_xi@163.com>
2019-07-10 00:02:29 +0800

    修正编译错误

--- TimeApp/src/service/restful/syncsev.ts
+++ TimeApp/src/service/restful/syncsev.ts
@@ -128,7 +128,7 @@ export class SyncRestful {
       let taskRunAt = {
         eventId: "WEBHOOK_GITHUB",
         filters: [
-          webhook: "github"
+          {webhook: "github"}
         ]
       };
 
@@ -179,7 +179,7 @@ export class SyncRestful {
       let taskRunAt = {
         eventId: "WEBHOOK_TRAVIS-CI",
         filters: [
-          webhook: "travis-ci"
+          {webhook: "travis-ci"}
         ]
       };
 
@@ -230,7 +230,7 @@ export class SyncRestful {
       let taskRunAt = {
         eventId: "WEBHOOK_FIR.IM",
         filters: [
-          webhook: "fir.im"
+          {webhook: "fir.im"}
         ]
       };
 
