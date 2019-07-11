43293a124dd342ba36e1a87865f6e9597394d750
leon_xi <leon_xi@163.com>
2019-07-11 16:42:45 +0800

    使用Node.JS crypto实现Secret

--- TimeApp/src/util/crypto-util.ts
+++ TimeApp/src/util/crypto-util.ts
@@ -1,7 +1,12 @@
-import { Crypto } from 'cryptojs';
-
 /** @hidden */
 export function getSecret(userid: string, saltlength: number = 16) {
-  let salt = Crypto.util.randomBytes(16);
-  return Crypto.PBKDF2(userid, salt, 16, { asString: true });
+  let salt = randomBytes(16);
+  return crypto.pbkdf2(userid, salt, 16, 32, 'sha1');
+}
+
+/** @hidden */
+export function randomBytes(n: number) {
+  for (let bytes = []; n > 0; n--)
+    bytes.push(Math.floor(Math.random() * 256));
+  return bytes;
 }
