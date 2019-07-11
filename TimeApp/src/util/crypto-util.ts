5270cc52a86e95444a6b6840eec99467b26a079d
leon_xi <leon_xi@163.com>
2019-07-11 17:14:49 +0800

    更新

--- TimeApp/src/util/crypto-util.ts
+++ TimeApp/src/util/crypto-util.ts
@@ -2,10 +2,12 @@ import { pbkdf2 } from "crypto";
 
 /** @hidden */
 export function getSecret(userid: string, saltlength: number = 16) {
-  var salt = randomBytes(16);
+  return new Promise<any>(async (resolve, reject) => {
+    var salt = randomBytes(16);
 
-  pbkdf2(userid, salt, 16, 32, 'sha1', (err, deliverykey) => {
-    return deliverykey;
+    pbkdf2(userid, salt, 16, 32, 'sha1', (err, deliverykey) => {
+      return deliverykey;
+    });
   });
 }
 
