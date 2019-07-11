a21216c3ce27a0bf59303e8566466b4fe8f2deee
leon_xi <leon_xi@163.com>
2019-07-11 16:52:42 +0800

    更新crypto

--- TimeApp/src/util/crypto-util.ts
+++ TimeApp/src/util/crypto-util.ts
@@ -1,12 +1,14 @@
+import { crypto } from "crypto";
+
 /** @hidden */
 export function getSecret(userid: string, saltlength: number = 16) {
-  let salt = randomBytes(16);
+  var salt = randomBytes(16);
   return crypto.pbkdf2(userid, salt, 16, 32, 'sha1');
 }
 
 /** @hidden */
 export function randomBytes(n: number) {
-  for (let bytes = []; n > 0; n--)
+  for (var bytes = []; n > 0; n--)
     bytes.push(Math.floor(Math.random() * 256));
   return bytes;
 }
