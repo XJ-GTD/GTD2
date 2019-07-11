132e59a55a00e41d9163ba8461719fa7f7106583
leon_xi <leon_xi@163.com>
2019-07-11 16:55:32 +0800

    更新pbkdf2引用

--- TimeApp/src/util/crypto-util.ts
+++ TimeApp/src/util/crypto-util.ts
@@ -1,9 +1,9 @@
-import { crypto } from "crypto";
+import { pbkdf2 } from "crypto";
 
 /** @hidden */
 export function getSecret(userid: string, saltlength: number = 16) {
   var salt = randomBytes(16);
-  return crypto.pbkdf2(userid, salt, 16, 32, 'sha1');
+  return pbkdf2(userid, salt, 16, 32, 'sha1');
 }
 
 /** @hidden */
