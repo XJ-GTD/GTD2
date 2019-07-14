1fbb0de071662588049c4d5e2100e5cfc4a81878
leon_xi <leon_xi@163.com>
2019-07-14 14:06:13 +0800

    更新样式

--- TimeApp/src/pages/fo/foshare.ts
+++ TimeApp/src/pages/fo/foshare.ts
@@ -44,7 +44,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
         <ion-list no-lines>
           <ion-list-header>GitHub</ion-list-header>
           <ion-item>
-            <h2><ion-icon name="git-network"></ion-icon> XJ-GTD/GTD2</h2>
+            <h3><ion-icon name="git-network"></ion-icon> XJ-GTD/GTD2</h3>
             <p>过去, 现在和未来管理局 ( 记录过去，提醒现在和预约未来 )</p>
             <button ion-button clear item-end>
               <ion-icon ios="ios-add-circle-outline" md="ios-add-circle-outline"></ion-icon>
@@ -52,12 +52,12 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
           </ion-item>
           <ion-list-header>集成 | fir.im</ion-list-header>
           <ion-item>
-            <h2>
+            <h3>
               <ion-thumbnail>
                 <img src="http://firicon.fir.im/690cc493f7aa664d50c8a6493b964bc151181128?attname=blob&tmp=1563027675.485636">
               </ion-thumbnail>
                　 冥王星
-            </h2>
+            </h3>
             <p>Platform: Android</p>
             <button ion-button clear item-end>
               <ion-icon ios="ios-add-circle-outline" md="ios-add-circle-outline"></ion-icon>
