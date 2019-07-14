9eaf24270cfc0a47a9dd282db3d45a097880355e
leon_xi <leon_xi@163.com>
2019-07-14 17:29:02 +0800

    更新操作按钮

--- TimeApp/src/pages/fo/foshare.ts
+++ TimeApp/src/pages/fo/foshare.ts
@@ -43,23 +43,31 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
       <ion-row>
         <ion-list no-lines>
           <ion-list-header>GitHub</ion-list-header>
-          <ion-item>
-            <h3><ion-icon name="git-network"></ion-icon> XJ-GTD/GTD2</h3>
-            <p>过去, 现在和未来管理局 ( 记录过去，提醒现在和预约未来 )</p>
-            <button ion-button clear item-end>
-              <ion-icon ios="ios-add-circle-outline" md="ios-add-circle-outline"></ion-icon>
-            </button>
-          </ion-item>
-          <ion-item>
-            <ion-avatar item-start>
-              <img [src]="defaultavatar">
-            </ion-avatar>
-            <h3><ion-icon name="git-network"></ion-icon> XJ-GTD/GTD2</h3>
-            <p>过去, 现在和未来管理局 ( 记录过去，提醒现在和预约未来 )</p>
-            <button ion-button clear item-end>
-              关闭
-            </button>
-          </ion-item>
+          <ion-item-sliding>
+            <ion-item>
+              <h3><ion-icon name="git-network"></ion-icon> XJ-GTD/GTD2</h3>
+              <p>过去, 现在和未来管理局 ( 记录过去，提醒现在和预约未来 )</p>
+            </ion-item>
+            <ion-item-options side="right">
+              <button ion-button clear>
+                <ion-icon ios="ios-add-circle-outline" md="ios-add-circle-outline"></ion-icon>
+              </button>
+            </ion-item-options>
+          </ion-item-sliding>
+          <ion-item-sliding>
+            <ion-item>
+              <ion-avatar item-start>
+                <img [src]="defaultavatar">
+              </ion-avatar>
+              <h3><ion-icon name="git-network"></ion-icon> XJ-GTD/GTD2</h3>
+              <p>过去, 现在和未来管理局 ( 记录过去，提醒现在和预约未来 )</p>
+            </ion-item>
+            <ion-item-options side="right">
+              <button ion-button clear>
+                关闭
+              </button>
+            </ion-item-options>
+          </ion-item-sliding>
 
           <ion-list-header>集成 | fir.im</ion-list-header>
           <ion-item-sliding>
@@ -98,21 +106,25 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
               </button>
             </ion-item-options>
           </ion-item-sliding>
-          <ion-item>
-            <ion-avatar item-start>
-              <img [src]="defaultavatar">
-            </ion-avatar>
-            <h3>
-              <ion-thumbnail>
-                <img src="http://firicon.fir.im/690cc493f7aa664d50c8a6493b964bc151181128?attname=blob&tmp=1563027675.485636">
-              </ion-thumbnail>
-               　　冥王星
-            </h3>
-            <p>Platform: Android</p>
-            <button ion-button clear item-end>
-              关闭
-            </button>
-          </ion-item>
+          <ion-item-sliding>
+            <ion-item>
+              <ion-avatar item-start>
+                <img [src]="defaultavatar">
+              </ion-avatar>
+              <h3>
+                <ion-thumbnail>
+                  <img src="http://firicon.fir.im/690cc493f7aa664d50c8a6493b964bc151181128?attname=blob&tmp=1563027675.485636">
+                </ion-thumbnail>
+                 　　冥王星
+              </h3>
+              <p>Platform: Android</p>
+            </ion-item>
+            <ion-item-options side="right">
+              <button ion-button clear>
+                关闭
+              </button>
+            </ion-item-options>
+          </ion-item-sliding>
         </ion-list>
       </ion-row>
     </ion-grid>
