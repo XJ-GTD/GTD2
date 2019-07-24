bb149cd9525a2d8ff51635ffdd34fe0d548c8333
leon_xi <leon_xi@163.com>
2019-07-24 15:27:04 +0800

    更新

--- TimeApp/src/service/cordova/rabbitmq.service.ts
+++ TimeApp/src/service/cordova/rabbitmq.service.ts
@@ -34,19 +34,8 @@ export class RabbitMQService {
 
   //Native Call Function
   messageReceived(event) {
-    var cache = [];
-    console.log("RabbitMQ received message: " + JSON.stringify(event, function(key, value) {
-        if (typeof value === 'object' && value !== null) {
-            if (cache.indexOf(value) !== -1) {
-                // 移除
-                return;
-            }
-            // 收集所有的值
-            cache.push(value);
-        }
-        return value;
-    }));
-    cache = null; // 清空变量，便于垃圾回收机制回收
+    console.log("RabbitMQ received message: " + JSON.stringify(event['header']));
+    console.log("RabbitMQ received message: " + JSON.stringify(event['content']));
   }
 
   init(uid: string, deviceid: string, queuename: string) {
