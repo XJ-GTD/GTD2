7ef5e1d96c0230d639e200ba25da277baadeed11
leon_xi <leon_xi@163.com>
2019-07-30 18:34:18 +0800

    更新百度语音

--- BaiduSpeechAndTTS/src/android/com/xj/ionic/speechandtts/XjBaiduSpeech.java
+++ BaiduSpeechAndTTS/src/android/com/xj/ionic/speechandtts/XjBaiduSpeech.java
@@ -1,14 +1,11 @@
 package com.xj.ionic.speechandtts;
 
 import android.Manifest;
-import android.content.Context;
+import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import com.baidu.aip.asrwakeup3.core.recog.MyRecognizer;
-import com.baidu.aip.asrwakeup3.core.util.FileUtil;
-import com.baidu.speech.asr.SpeechConstant;
 import com.xj.ionic.speechandtts.listener.XjSpeechRecogListener;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -16,88 +13,64 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import cn.sh.com.xj.timeApp.R;
-
 
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class XjBaiduSpeech extends CordovaPlugin{
+public class XjBaiduSpeech extends CordovaPlugin {
 
-    private boolean isTs = true;
 
-    private String samplePath;
+    private  static MyRecognizer recongnizer;
+
+    private static Activity cordovaActivity;
+
+
 
     @Override
     protected void pluginInitialize() {
 
         super.pluginInitialize();
 
-        initPermission();
+        if (recongnizer == null){
 
-        initSamplePath();
+            initPermission();
 
-    }
+            recongnizer = new MyRecognizer(cordova.getActivity());
+        }
+        cordovaActivity = cordova.getActivity();
+
+        }
+
+
+
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        Map<String, Object> params = new LinkedHashMap<String, Object>();
-
-
-        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
-        params.put(SpeechConstant.NLU, "enable");
-        params.put(SpeechConstant.PID, 15372);
-        params.put(SpeechConstant.VAD, SpeechConstant.VAD_DNN);
-        params.put(SpeechConstant.ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH, "assets://baidu_speech_grammar.bsg");
-        params.put(SpeechConstant.OUT_FILE, samplePath+ "/iat.pcm");
-        params.put(SpeechConstant.ACCEPT_AUDIO_DATA, "true");
-
-        if (isTs){
-            params.put(SpeechConstant.SOUND_START, R.raw.bdspeech_recognition_start);
-            params.put(SpeechConstant.SOUND_END, R.raw.bdspeech_speech_end);
-            params.put(SpeechConstant.SOUND_SUCCESS, R.raw.bdspeech_recognition_success);
-            params.put(SpeechConstant.SOUND_ERROR, R.raw.bdspeech_recognition_error);
-            params.put(SpeechConstant.SOUND_CANCEL, R.raw.bdspeech_recognition_cancel);
-
-        }
-        //params.put(SpeechConstant.OUT_FILE, "res://msc/iat.wav");
-        // SpeechConstant.SOUND_START,
-        //params.put(SpeechConstant.VAD_ENDPOINOUTT_TIMEOUT, 0); // 长语音
-        // params.put(SpeechConstant.IN_FILE, "res:///com/baidu/android/voicedemo/16k_test.pcm");
-        // params.put(SpeechConstant.PROP ,20000);
-        // params.put(SpeechConstant.PID, 1537); // 中文输入法模型，有逗号
-
-
-        MyRecognizer recongnizer = new MyRecognizer(cordova.getActivity(),new XjSpeechRecogListener(callbackContext));
-
-
+        recongnizer.setEventListener(new XjSpeechRecogListener(callbackContext));
+        cordovaActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
         if (action.equals("start")) {
-            recongnizer.start(params);
-            return true;
-        }else if(action.equals("stop")){
+                    recongnizer.start();
+                } else if (action.equals("stop")) {
             recongnizer.stop();
 
-        }else if(action.equals("cancel")){
+                } else if (action.equals("cancel")) {
             recongnizer.cancel();
-            return true;
-        }else if(action.equals("release")){
+                } else if (action.equals("release")) {
             recongnizer.release();
-            return true;
         }
-        return false;
+    }
+        });
+
+
+
+        return true;
     }
 
-    private void initSamplePath() {
-        String sampleDir = "";
-        samplePath = this.cordova.getActivity().getCacheDir().getPath() + "/" + sampleDir;
-        if (!FileUtil.makeDir(samplePath)) {
-            throw new RuntimeException("创建临时目录失败 :" + samplePath);
-        }
-    }
+
 
 
     /**
