de3a846290e86dd7e1a437b427497fb01e6f45e9
leon_xi <leon_xi@163.com>
2019-08-01 15:13:39 +0800

    更新样式

--- TimeApp/src/components/recording/recording.ts
+++ TimeApp/src/components/recording/recording.ts
@@ -4,21 +4,21 @@ import { Component, Input, Output, ElementRef, ViewChild, EventEmitter } from "@
 @Component({
   selector: 'recording',
   template: `<ion-grid [ngClass]="{'active': active}">
-    <ion-row>
+    <ion-row justify-content-center>
       <ion-icon ios="ios-microphone" md="ios-microphone"></ion-icon>
-      <svg width="50" height="56" xmlns="http://www.w3.org/2000/svg">
+      <svg width="32" height="56" xmlns="http://www.w3.org/2000/svg">
        <g>
         <title>Layer 1</title>
-        <rect id="svg_1" height="2" width="3" y="45.63281" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_2" height="2" width="6" y="41.67448" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_3" height="2" width="9" y="37.71615" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_4" height="2" width="12" y="33.75781" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_5" height="2" width="15" y="29.59115" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_6" height="2" width="18" y="25.42448" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_7" height="2" width="21" y="21.25781" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_8" height="2" width="24" y="17.09115" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_9" height="2" width="27" y="12.71615" x="10" stroke="#fff" fill="#fff"/>
-        <rect id="svg_10" height="2" width="30" y="8.34115" x="10" stroke="#fff" fill="#fff"/>
+        <rect id="svg_1" height="2" width="3" y="45.63281" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_2" height="2" width="6" y="41.67448" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_3" height="2" width="9" y="37.71615" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_4" height="2" width="12" y="33.75781" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_5" height="2" width="15" y="29.59115" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_6" height="2" width="18" y="25.42448" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_7" height="2" width="21" y="21.25781" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_8" height="2" width="24" y="17.09115" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_9" height="2" width="27" y="12.71615" x="1" stroke="#fff" fill="#fff"/>
+        <rect id="svg_10" height="2" width="30" y="8.34115" x="1" stroke="#fff" fill="#fff"/>
        </g>
       </svg>
     </ion-row>
