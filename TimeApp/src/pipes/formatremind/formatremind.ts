622f7d4eede516dd5571e58cc5024ad49e20dbb1
leon_xi <leon_xi@163.com>
2019-08-01 21:03:14 +0800

    数字类型提醒

--- TimeApp/src/pipes/formatremind/formatremind.ts
+++ TimeApp/src/pipes/formatremind/formatremind.ts
@@ -13,32 +13,32 @@ export class FormatRemindPipe implements PipeTransform {
    * Takes a value and makes it lowercase.
    */
   transform(value: any, ...args) {
-    if (value == "" || value == "0"){
+    if (!value || value == 0 || value == "" || value == "0"){
       return value;
     }
 
-    let repeat: string = "不可用";
+    let remind: string = "不可用";
 
     switch(value) {
-      case "1" :
-        repeat = "提前10分钟提醒";
+      case 1 :
+        remind = "提前10分钟提醒";
         break;
-      case "2" :
-        repeat = "提前30分钟提醒";
+      case 2 :
+        remind = "提前30分钟提醒";
         break;
-      case "3" :
-        repeat = "提前1小时提醒";
+      case 3 :
+        remind = "提前1小时提醒";
         break;
-      case "4" :
-        repeat = "提前4小时提醒";
+      case 4 :
+        remind = "提前4小时提醒";
         break;
-      case "5" :
-        repeat = "提前1天提醒";
+      case 5 :
+        remind = "提前1天提醒";
         break;
       default :
-        repeat = "不可用";
+        remind = "不可用";
     }
 
-    return repeat;
+    return remind;
   }
 }
