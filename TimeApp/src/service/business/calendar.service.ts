114e1f67c4a9d198637f17fd1ef8989382cea79b
leon_xi <leon_xi@163.com>
2019-08-06 16:12:13 +0800

    更新方法实现

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -1,16 +1,76 @@
 import { Injectable } from "@angular/core";
 import { SqliteExec } from "../util-service/sqlite.exec";
 import { UtilService } from "../util-service/util.service";
+import { EmitService } from "../util-service/emit.service";
 
 @Injectable()
 export class CalendarService {
   constructor(private sqlExce: SqliteExec,
-              private util: UtilService) {}
+              private util: UtilService,
+              private emitService: EmitService) {}
 
-  savePlan() {}
+  /**
+   * 创建/更新日历
+   *
+   * @author leon_xi@163.com
+   **/
+  async savePlan(plan: PlanData): PlanData {
+
+    if (plan.ji) {
+      // 更新
+      let plandb: JhTbl = new JhTbl();
+      Object.assign(plandb, plan);
+
+      await this.sqlExce.update(plandb);
+
+      this.emitService.emit(`mwxing.calendar.${plan.ji}.updated`);
+    } else {
+      // 新建
+      plan.ji = this.util.getUuid();
+
+      let plandb: JhTbl = new JhTbl();
+      Object.assign(plandb, plan);
+
+      await this.sqlExce.insert(plandb);
+
+      this.emitService.emit(`mwxing.calendar.plan.created`);
+    }
+
+    return plan;
+  }
+
   updatePlanColor() {}
   removePlan() {}
-  savePlanItem() {}
+
+  /**
+   * 创建/更新日历项
+   *
+   * @author leon_xi@163.com
+   **/
+  async savePlanItem(item: PlanItemData) {
+    if (item.jti) {
+      // 更新
+      let planitemdb: JhiTbl = new JhiTbl();
+      Object.assign(planitemdb, item);
+
+      await this.sqlExce.update(planitemdb);
+
+      this.emitService.emit(`mwxing.calendar.${item.ji}.item.updated`);
+    } else {
+      // 新建
+      item.jti = this.util.getUuid();
+
+      let planitemdb: JhiTbl = new JhiTbl();
+      Object.assign(planitemdb, item);
+
+      await this.sqlExce.insert(planitemdb);
+
+      this.emitService.emit(`mwxing.calendar.${item.ji}.item.created`);
+    }
+
+    return plan;
+  }
+
   removePlanItem() {}
   fetchAllPlans() {}
   fetchPrivatePlans() {}
