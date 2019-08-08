38a22764288b9d8f92cd9cda4eec113e2a0a0cbf
leon_xi <leon_xi@163.com>
2019-08-08 18:24:51 +0800

    增加测试Case

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -5,6 +5,7 @@ import { CalendarService, PlanData } from "./calendar.service";
 
 describe('CalendarService test suite', () => {
   let calendarService: CalendarService;
+  let planforUpdate: PlanData;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,14 +18,31 @@ describe('CalendarService test suite', () => {
     expect(calendarService).toBeTruthy();
   });
 
-  it('', async(() => {
+  it('Case 1 - 2 use savePlan to create a new plan', async(() => {
     let plan: PlanData = {} as PlanData;
 
     plan.jn = '测试日历';
     plan.jc = '#f1f1f1';
 
     calendarService.savePlan(plan).then(savedPlan => {
+      planforUpdate = savedPlan;  // 保存用于后面的测试用例
+
       expect(savedPlan.ji).toBeDefined();
+      expect(savedPlan.ji).not.toBe('');
     });
+  }));
+
+  it('Case 1 - 3 use savePlan to update an exist plan\'s color', async(() => {
+    if (planforUpdate && planforUpdate.ji) {
+      let plan: PlanData = planforUpdate;
+
+      plan.jc = '#1a1a1a';
+
+      calendarService.savePlan(plan).then(savedPlan => {
+        planforUpdate = savedPlan;  // 保存用于后面的测试用例
+
+        expect(savedPlan.jc).toBe('#1a1a1a');
+      });
+    }
   }));
 });
