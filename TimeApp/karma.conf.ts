926792735f157cc2a222e3572596cad349be0130
leon_xi <leon_xi@163.com>
2019-08-08 23:13:44 +0800

    更新

--- TimeApp/karma.conf.ts
+++ TimeApp/karma.conf.ts
@@ -43,7 +43,7 @@ module.exports = (config) => {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      './src/test.ts': ['@angular-devkit/build-angular']
     },
 
 
