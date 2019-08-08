afd818b8ae2ee95ccec5febb5130dca0422e6c41
leon_xi <leon_xi@163.com>
2019-08-08 14:58:08 +0800

    增加jasmine unit test

---
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalendarService } from "./calendar.service";
+
+describe ('Basic test sample', () => {
+  let calendarService: CalendarService;
+
+  beforeEach(() => {
+    calendarService = new CalendarService();
+  });
+
+  it('test a', () => {
+
+  });
+});
