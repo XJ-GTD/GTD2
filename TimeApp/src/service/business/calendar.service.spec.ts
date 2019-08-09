1728b35c8f09b13178ff7c607380e0ac94f6c7e3
leon_xi <leon_xi@163.com>
2019-08-09 16:14:57 +0800

    增加项目引用

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -1,16 +1,20 @@
 import {} from 'jasmine';
 import { TestBed, async } from '@angular/core/testing';
 
+import { IonicModule, Platform } from 'ionic-angular';
+import {HttpClientModule} from "@angular/common/http";
+import {SQLite} from '@ionic-native/sqlite';
+import {SQLitePorter} from '@ionic-native/sqlite-porter';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+
 import {
   PlatformMock,
   StatusBarMock,
   SplashScreenMock
 } from '../../../test-config/mocks-ionic';
 
-import {SQLite} from '@ionic-native/sqlite';
-import {SQLitePorter} from '@ionic-native/sqlite-porter';
-import {StatusBar} from '@ionic-native/status-bar';
-import {SplashScreen} from '@ionic-native/splash-screen';
+import {MyApp} from '../../app/app.component';
 
 import {SqliteConfig} from "../config/sqlite.config";
 import {RestfulClient} from "../util-service/restful.client";
@@ -27,6 +31,9 @@ import { CalendarService, PlanData, PlanType } from "./calendar.service";
  * 日历Service 持续集成CI 自动测试Case
  * 确保问题修复的过程中, 保持原有逻辑的稳定
  *
+ * 使用 karma jasmine 进行 unit test
+ * 参考: https://github.com/ionic-team/ionic-unit-testing-example
+ *
  * @author leon_xi@163.com
  **/
 describe('CalendarService test suite', () => {
@@ -35,6 +42,13 @@ describe('CalendarService test suite', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      declarations: [
+        MyApp
+      ],
+      imports: [
+        IonicModule.forRoot(MyApp),
+        HttpClientModule
+      ],
       providers: [
         SQLite,
         SQLitePorter,
@@ -47,7 +61,8 @@ describe('CalendarService test suite', () => {
         ShaeRestful,
         CalendarService,
         { provide: StatusBar, useClass: StatusBarMock },
-        { provide: SplashScreen, useClass: SplashScreenMock }
+        { provide: SplashScreen, useClass: SplashScreenMock },
+        { provide: Platform, useClass: PlatformMock }
       ]
     });
     calendarService = TestBed.get(CalendarService);
