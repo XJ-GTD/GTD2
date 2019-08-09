22b6fbfc65ca10147ab5d4e0df56abe9d99f53ca
leon_xi <leon_xi@163.com>
2019-08-09 16:00:53 +0800

    更新测试Case顺序

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -53,9 +53,19 @@ describe('CalendarService test suite', () => {
     calendarService = TestBed.get(CalendarService);
   });
 
-  it('Case 1 - 1 service should be created', () => {
-    expect(calendarService).toBeTruthy();
-  });
+  it('Case 1 - 3 use savePlan to update an exist plan\'s color', async(() => {
+    if (planforUpdate && planforUpdate.ji) {
+      let plan: PlanData = planforUpdate;
+
+      plan.jc = '#1a1a1a';
+
+      calendarService.savePlan(plan).then(savedPlan => {
+        planforUpdate = savedPlan;  // 保存用于后面的测试用例
+
+        expect(savedPlan.jc).toBe('#1a1a1a');
+      });
+    }
+  }));
 
   it('Case 1 - 2 use savePlan to create a new plan', async(() => {
     let plan: PlanData = {} as PlanData;
@@ -72,17 +82,7 @@ describe('CalendarService test suite', () => {
     });
   }));
 
-  it('Case 1 - 3 use savePlan to update an exist plan\'s color', async(() => {
-    if (planforUpdate && planforUpdate.ji) {
-      let plan: PlanData = planforUpdate;
-
-      plan.jc = '#1a1a1a';
-
-      calendarService.savePlan(plan).then(savedPlan => {
-        planforUpdate = savedPlan;  // 保存用于后面的测试用例
-
-        expect(savedPlan.jc).toBe('#1a1a1a');
-      });
-    }
-  }));
+  it('Case 1 - 1 service should be created', () => {
+    expect(calendarService).toBeTruthy();
+  });
 });
