5bf0c7881a940879b23c135ee233534ebe43eecf
leon_xi <leon_xi@163.com>
2019-08-09 14:51:32 +0800

    增加core-js

--- TimeApp/test-config/karma-test-shim.js
+++ TimeApp/test-config/karma-test-shim.js
@@ -1,7 +1,7 @@
 Error.stackTraceLimit = Infinity;
 
-require('core-js/es5');
-require('core-js/es5/reflect');
+require('core-js/es6');
+require('core-js/es7/reflect');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
