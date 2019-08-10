e69f0f9f71962dc9fad78c86abf524084c0fc911
leon_xi <leon_xi@163.com>
2019-08-10 16:01:53 +0800

    更新sqlite测试case

---
+++ TimeApp/src/service/util-service/sqlite.exec.spec.ts
@@ -0,0 +1,63 @@
+import {} from 'jasmine';
+import { TestBed, async } from '@angular/core/testing';
+
+import {Device} from "@ionic-native/device";
+import {
+  IonicModule,
+  Platform,
+  AlertController,
+  LoadingController,
+  PopoverController,
+  ToastController
+} from "ionic-angular";
+import {SQLitePorter} from "@ionic-native/sqlite-porter";
+import {SqliteConfig} from "../config/sqlite.config";
+
+import {UtilService} from "../util-service/util.service";
+import { SqliteExec } from "./sqlite.exec";
+
+/**
+ * Sqlite工具类 持续集成CI 自动测试Case
+ * 确保问题修复的过程中, 保持原有逻辑的稳定
+ *
+ * 使用 karma jasmine 进行 unit test
+ * 参考: https://github.com/ionic-team/ionic-unit-testing-example
+ *
+ * @author leon_xi@163.com
+ **/
+describe('SqliteExec test suite', () => {
+  let sqlExce: SqliteExec;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        IonicModule.forRoot()
+      ],
+      providers: [
+        Device,
+        AlertController,
+        LoadingController,
+        PopoverController,
+        ToastController,
+        UtilService,
+        SQLitePorter,
+        SqliteConfig,
+        SqliteExec
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    sqlExce = TestBed.get(SqliteExec);
+  });
+
+  it('Case 1 - 1 create() without Error', () => {
+    expect(function() {
+      let y: YTbl = new YTbl();
+      sqlExce.create(y);
+    }).not.toThrow();
+  });
+
+  afterAll(() => {
+    TestBed.resetTestingModule();
+  });
+});
