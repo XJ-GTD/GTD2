95a4dc8cba1d56da4ae1cfa0635ed1adcdebb927
leon_xi <leon_xi@163.com>
2019-08-10 16:36:25 +0800

    更新测试用例说明

--- TimeApp/src/service/util-service/util.service.spec.ts
+++ TimeApp/src/service/util-service/util.service.spec.ts
@@ -44,7 +44,7 @@ describe('UtilService test suite', () => {
     utilService = TestBed.get(UtilService);
   });
 
-  it(`Case 3 - 1 mask 13387322344 => 133****2344`, () => {
+  it(`Case 3 - 1 mask('13387322344', 3, 4) => 133****2344`, () => {
     // 方法调用使用返回结果进行返回值判断
     let result = utilService.mask('13387322344', 3, 4);
 
