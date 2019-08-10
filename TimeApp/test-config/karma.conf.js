c5a9cc8523242bf4b602eea604175c9e3010342d
leon_xi <leon_xi@163.com>
2019-08-10 10:34:58 +0800

    更新

--- TimeApp/test-config/karma.conf.js
+++ TimeApp/test-config/karma.conf.js
@@ -54,8 +54,8 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   };
 
   config.set(_config);
