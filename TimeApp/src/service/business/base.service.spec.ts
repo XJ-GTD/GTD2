6010c56dee68a4962cd7463b7920450e1c12e1b9
leon_xi <leon_xi@163.com>
2019-08-10 10:32:21 +0800

    更新BaseService测试

---
+++ TimeApp/src/service/business/base.service.spec.ts
@@ -0,0 +1,30 @@
+import {} from 'jasmine';
+import { TestBed, async } from '@angular/core/testing';
+
+import { BaseService } from "./base.service";
+
+/**
+ * 日历Service 持续集成CI 自动测试Case
+ * 确保问题修复的过程中, 保持原有逻辑的稳定
+ *
+ * 使用 karma jasmine 进行 unit test
+ * 参考: https://github.com/ionic-team/ionic-unit-testing-example
+ *
+ * @author leon_xi@163.com
+ **/
+describe('BaseService test suite', () => {
+  let baseService: BaseService;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [BaseService]
+    });
+  }));
+
+  beforeEach(() => {
+    baseService = TestBed.get(BaseService);
+  });
+
+  it('Base test', () => {
+    expect(baseService.assertEmpty(null)).toThrowAnyError();
+  });
+});
