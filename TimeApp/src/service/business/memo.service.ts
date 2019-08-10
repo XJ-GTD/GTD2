a4980855b3ed04240d4278fc4b395e73a9c8003c
“ying <343253410@qq.com>
2019-08-10 09:51:21 +0800

    备忘服务的备份和恢复实现

--- TimeApp/src/service/business/memo.service.ts
+++ TimeApp/src/service/business/memo.service.ts
@@ -3,6 +3,9 @@ import { BaseService } from "./base.service";
 import { SqliteExec } from "../util-service/sqlite.exec";
 import { UtilService } from "../util-service/util.service";
 import { MomTbl } from "../sqlite/tbl/mom.tbl";
+import { BackupPro, BacRestful, OutRecoverPro, RecoverPro } from "../../service/restful/bacsev";
+import {UserConfig} from "../../service/config/user.config";
+import * as moment from "moment";
 
 @Injectable()
 export class MemoService extends BaseService {
@@ -33,12 +36,12 @@ export class MemoService extends BaseService {
 	/**
 	 *  更新备忘计划,只是更新计划ID
 	 */
-	async updateMemoPlan(ji: string,moi:string){
+	async updateMemoPlan(ji: string, moi: string) {
 		this.assertEmpty(ji); // 计划ID不能为空
 		this.assertEmpty(moi); // 备案ID不能为空
 		let memodb: MomTbl = new MomTbl();
-		memodb.moi=moi;
-		memodb.ji=ji;
+		memodb.moi = moi;
+		memodb.ji = ji;
 		await this.sqlExce.updateByParam(memodb);
 	}
 	/**
@@ -53,9 +56,9 @@ export class MemoService extends BaseService {
 		//删除备忘相关的附件
 		sqls.push(`delete * from gtd_fj where obt = '${ObjectType.Calendar}' and obi ='${moi}';`);
 		//删除备忘相关的标签
-		sqls.push(`delete * from gtd_mk where obt = '${ObjectType.Calendar}' and obi ='${moi}';`);  
+		sqls.push(`delete * from gtd_mk where obt = '${ObjectType.Calendar}' and obi ='${moi}';`);
 		//删除参与人表
-		sqls.push(`delete * from gtd_d where obt = '${ObjectType.Calendar}' and obi ='${moi}';`); 
+		sqls.push(`delete * from gtd_d where obt = '${ObjectType.Calendar}' and obi ='${moi}';`);
 		await this.sqlExce.batExecSqlByParam(sqls);
 	}
 	/**
@@ -81,11 +84,42 @@ export class MemoService extends BaseService {
 	/**
 	 * 备份备忘到服务器
 	 */
-	backup() {}
+	async backup() {
+		let backupPro: BackupPro = new BackupPro();
+		//操作账户ID
+		backupPro.oai = UserConfig.account.id
+		//操作手机号码
+		backupPro.ompn = UserConfig.account.phone;
+		//时间戳
+		backupPro.d.bts = moment().unix();
+		let mom = new MomTbl();
+		//TODO 此处是否要新增逻辑判断只有未同步的数据,才会同步到服务器,暂时不做处理,后续变动
+		backupPro.d.memo = await this.sqlexec.getList < MomTbl > (mom);
+		await this.bacRestful.backup(backupPro);
+	}
 	/**
 	 * 恢复备忘,根据服务器同步到客户端
 	 */
-	recovery() {}
+	async recovery(bts: Number) {
+		let recoverPro: RecoverPro = new RecoverPro();
+		//操作账户ID
+		recoverPro.oai = UserConfig.account.id;
+		//操作手机号码
+		recoverPro.ompn = UserConfig.account.phone;
+		recoverPro.d.bts = bts;
+		let outRecoverPro: OutRecoverPro = await this.bacRestful.recover(recoverPro);
+		let mom = new MomTbl();
+		let sqls = new Array < string > ();
+		//先删除
+		await this.sqlexec.delete(mom);
+		//在同步新的数据
+		for(let j = 0, len = outRecoverPro.memo.length; j < len; j++) {
+			let moi = new MomTbl();
+			Object.assign(moi, outRecoverPro.memo[j]);
+			sqls.push(moi.inTParam());
+		}
+		await this.sqlexec.batExecSql(sqls);
+	}
 }
 
 export interface MemoData extends MomTbl {
@@ -93,7 +127,7 @@ export interface MemoData extends MomTbl {
 }
 
 export enum ObjectType {
-  Event = 'event',
-  Memo = 'memo',
-  Calendar = 'calendar'
+	Event = 'event',
+		Memo = 'memo',
+		Calendar = 'calendar'
 }
