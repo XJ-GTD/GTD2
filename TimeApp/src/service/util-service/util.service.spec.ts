400457fba85a1575d1a113982f5da1d72086d097
leon_xi <leon_xi@163.com>
2019-08-10 13:54:05 +0800

    增加UtilService测试case

---
+++ TimeApp/src/service/util-service/util.service.spec.ts
@@ -0,0 +1,36 @@
+import {} from 'jasmine';
+import { TestBed, async } from '@angular/core/testing';
+
+import { UtilService } from "./util.service";
+
+/**
+ * 通用工具Service父类 持续集成CI 自动测试Case
+ * 确保问题修复的过程中, 保持原有逻辑的稳定
+ *
+ * 使用 karma jasmine 进行 unit test
+ * 参考: https://github.com/ionic-team/ionic-unit-testing-example
+ *
+ * @author leon_xi@163.com
+ **/
+describe('UtilService test suite', () => {
+  let utilService: UtilService;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [UtilService]
+    });
+  }));
+
+  beforeEach(() => {
+    utilService = TestBed.get(UtilService);
+  });
+
+  it('Case 1 - 1 assertEmpty(null)', () => {
+    expect(function() {
+      utilService.rand(0, 10);
+    }).not.toThrow();
+  });
+
+  afterAll(() => {
+    TestBed.resetTestingModule();
+  });
+});
