091526e66258380f6428c0383a35eed64db41e89
leon_xi <leon_xi@163.com>
2019-08-10 18:37:51 +0800

    增加CalendarService测试Case

---
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -0,0 +1,86 @@
+import {} from 'jasmine';
+import { TestBed, async } from '@angular/core/testing';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import { Platform } from 'ionic-angular';
+
+import {
+  PlatformMock,
+  StatusBarMock,
+  SplashScreenMock
+} from '../../../test-config/mocks-ionic';
+
+import { CalendarService, PlanData, PlanType } from "./calendar.service";
+
+/**
+ * 日历Service 持续集成CI 自动测试Case
+ * 确保问题修复的过程中, 保持原有逻辑的稳定
+ *
+ * 使用 karma jasmine 进行 unit test
+ * 参考: https://github.com/ionic-team/ionic-unit-testing-example
+ *
+ * @author leon_xi@163.com
+ **/
+describe('CalendarService test suite', () => {
+  //let fixture;
+  //let initPage: AlPage;
+  let calendarService: CalendarService;
+  let planforUpdate: PlanData;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MyApp
+      ],
+      imports: [
+        IonicModule.forRoot(MyApp)
+      ],
+      providers: [
+        CalendarService,
+        { provide: StatusBar, useClass: StatusBarMock },
+        { provide: SplashScreen, useClass: SplashScreenMock },
+        { provide: Platform, useClass: PlatformMock }
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    //fixture = TestBed.createComponent(AlPage);
+    //initPage = fixture.componentInstance;
+    //initPage.alinit();
+    calendarService = TestBed.get(CalendarService);
+  });
+
+  it('Case 1 - 3 use savePlan to update an exist plan\'s color', async(() => {
+    if (planforUpdate && planforUpdate.ji) {
+      let plan: PlanData = planforUpdate;
+
+      plan.jc = '#1a1a1a';
+
+      calendarService.savePlan(plan).then(savedPlan => {
+        planforUpdate = savedPlan;  // 保存用于后面的测试用例
+
+        expect(savedPlan.jc).toBe('#1a1a1a');
+      });
+    }
+  }));
+
+  it('Case 1 - 2 use savePlan to create a new plan', async(() => {
+    let plan: PlanData = {} as PlanData;
+
+    plan.jn = '冥王星服务类 自动测试';
+    plan.jc = '#f1f1f1';
+    plan.jt = PlanType.PrivatePlan;
+
+    calendarService.savePlan(plan).then(savedPlan => {
+      planforUpdate = savedPlan;  // 保存用于后面的测试用例
+
+      expect(savedPlan.ji).toBeDefined();
+      expect(savedPlan.ji).not.toBe('');
+    });
+  }));
+
+  it('Case 1 - 1 service should be created', () => {
+    expect(calendarService).toBeTruthy();
+  });
+});
