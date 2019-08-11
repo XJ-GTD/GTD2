b80c293b17fea6c6f57b04cb199b24922b47bc7d
leon_xi <leon_xi@163.com>
2019-08-11 12:47:59 +0800

    更新测试case

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -198,7 +198,7 @@ describe('CalendarService test suite', () => {
 
       if (plan) {
         expect(plan.ji).toBeDefined();
-        expect(plan.jc).toBe('#1a1a1a');
+        expect(plan.jc).toBe('#f1f1f1');
       }
     }
   });
