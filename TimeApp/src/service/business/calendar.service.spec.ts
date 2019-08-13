335fc245d8d22aaaecb04a6463be1e300d8b19a8
leon_xi <leon_xi@163.com>
2019-08-13 17:15:04 +0800

    更新测试case

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -50,7 +50,7 @@ import {FjTbl} from "../sqlite/tbl/fj.tbl";
 import { CalendarService, PlanData, PlanItemData, MonthActivityData, MonthActivitySummaryData, DayActivitySummaryData } from "./calendar.service";
 import { EventService, AgendaData } from "./event.service";
 import { MemoService, MemoData } from "./memo.service";
-import { PlanType } from "../../data.enum";
+import { PlanType, PlanItemType } from "../../data.enum";
 
 /**
  * 日历Service 持续集成CI 自动测试Case
@@ -187,12 +187,31 @@ describe('CalendarService test suite', () => {
 
   it(`Case 4 - 3 fetchDayActivitiesSummary 取得指定日期概要 - 存在1个日历项(任意日历项)`, async () => {
     let day: string = moment().format("YYYY/MM/DD");
+    // 基本日历
+    let plan: PlanData = {} as PlanData;
+
+    plan.jn = '农历节气 自动测试';
+    plan.jc = '#a1a1a1';
+    plan.jt = PlanType.CalendarPlan;
+
+    plan = await calendarService.savePlan(plan);
+
+    // 基本日历项
+    let planitem: PlanItemData = {} as PlanItemData;
+
+    planitem.ji = plan.ji;
+    planitem.sd = "2019/08/11";
+    planitem.jtn = "谷雨";
+    planitem.jtt = PlanItemType.Holiday;
+
+    await calendarService.savePlanItem(planitem);
 
     // 日历项
-    let planitem: PlanItemData = {} as PlanItemData;
+    planitem = {} as PlanItemData;
 
     planitem.sd = day;
     planitem.jtn = "结婚纪念日";
+    planitem.jtt = PlanItemType.Activity;
 
     await calendarService.savePlanItem(planitem);
 
@@ -200,7 +219,7 @@ describe('CalendarService test suite', () => {
 
     expect(daySummary).toBeDefined();
     expect(daySummary.day).toBe(day);
-    expect(daySummary.calendaritemscount).toBe(0);
+    expect(daySummary.calendaritemscount).toBe(1);
     expect(daySummary.activityitemscount).toBe(1);
     expect(daySummary.eventscount).toBe(0);
     expect(daySummary.agendascount).toBe(0);
