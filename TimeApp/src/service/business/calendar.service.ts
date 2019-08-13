db45775bfecad0b020182f51192df7911d0e5f49
leon_xi <leon_xi@163.com>
2019-08-13 17:26:12 +0800

    更新代码问题

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -467,7 +467,7 @@ export class CalendarService extends BaseService {
                               sum(CASE gev.type WHEN '${EventType.Agenda}' THEN 1 ELSE 0 END) agendascount,
                               sum(CASE gev.type WHEN '${EventType.Task}' THEN 1 ELSE 0 END) taskscount,
                               count(gmo.moi) memoscount,
-                              sum(CASE gev.rtevi WHEN NULL THEN 0 ELSE 1 END) repeateventscount,
+                              sum(CASE gev.rtevi WHEN NULL OR '' THEN 0 ELSE 1 END) repeateventscount,
                               0 bookedtimesummary
                       from (${daysql}) gday
                           left join gtd_jta gjt on gday.sd = gjt.sd
