3c0dcbc794d3e25832a5d1e20ce45c60d2ebafb8
leon_xi <leon_xi@163.com>
2019-08-14 18:56:26 +0800

    更新代码问题

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -555,26 +555,30 @@ export class CalendarService extends BaseService {
 
     this.assertEmpty(day);    // 入参不能为空
 
-    let sql: string = `select gdayev.*,
+    let sql: string = `select gdayev.day day,
+                              max(gdayev.calendaritemscount) calendaritemscount,
+                              max(gdayev.activityitemscount) activityitemscount,
+                              max(gdayev.eventscount) eventscount,
+                              max(gdayev.agendascount) agendascount,
+                              max(gdayev.taskscount) taskscount,
+                              max(gdayev.repeateventscount) repeateventscount,
                               sum(CASE IFNULL(gmo.moi, '') WHEN '' THEN 0 ELSE 1 END) memoscount,
                               0 bookedtimesummary
-                      from (
-                        select gdayjta.*,
-                              sum(CASE IFNULL(gev.evi, '') WHEN '' THEN 0 ELSE 1 END) eventscount,
-                              sum(CASE gev.type WHEN '${EventType.Agenda}' THEN 1 ELSE 0 END) agendascount,
-                              sum(CASE gev.type WHEN '${EventType.Task}' THEN 1 ELSE 0 END) taskscount,
-                              sum(CASE IFNULL(gev.rtevi, '') WHEN '' THEN 0 ELSE 1 END) repeateventscount
-                        from (
-                          select gday.sd day,
-                            sum(CASE gjt.jtt WHEN '${PlanItemType.Holiday}' THEN 1 ELSE 0 END) calendaritemscount,
-                            sum(CASE gjt.jtt WHEN '${PlanItemType.Activity}' THEN 1 ELSE 0 END) activityitemscount
-                          from (select '${day}' sd) gday
-                              left join gtd_jta gjt on gday.sd = gjt.sd
-                          group by gday.sd
-                          ) gdayjta
-                          left join gtd_ev gev on gdayjta.day = gev.evd
-                        group by gdayjta.day
-                        ) gdayev
+                      from (select gdayjta.day day,
+                                  max(gdayjta.calendaritemscount) calendaritemscount,
+                                  max(gdayjta.activityitemscount) activityitemscount,
+                                  sum(CASE IFNULL(gev.evi, '') WHEN '' THEN 0 ELSE 1 END) eventscount,
+                                  sum(CASE gev.type WHEN '${EventType.Agenda}' THEN 1 ELSE 0 END) agendascount,
+                                  sum(CASE gev.type WHEN '${EventType.Task}' THEN 1 ELSE 0 END) taskscount,
+                                  sum(CASE IFNULL(gev.rtevi, '') WHEN '' THEN 0 ELSE 1 END) repeateventscount
+                            from (select gday.sd day,
+                                    sum(CASE gjt.jtt WHEN '${PlanItemType.Holiday}' THEN 1 ELSE 0 END) calendaritemscount,
+                                    sum(CASE gjt.jtt WHEN '${PlanItemType.Activity}' THEN 1 ELSE 0 END) activityitemscount
+                                  from (select '${day}' sd) gday
+                                      left join gtd_jta gjt on gday.sd = gjt.sd
+                                  group by gday.sd) gdayjta
+                              left join gtd_ev gev on gdayjta.day = gev.evd
+                            group by gdayjta.day) gdayev
                       left join gtd_mom gmo on gdayev.day = gmo.sd
                       group by gdayev.day`;
 
