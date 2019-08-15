6ccadbe4d17ef04642fef50c92b0aff847cfe47f
leon_xi <leon_xi@163.com>
2019-08-15 19:05:25 +0800

    增加测试case

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -1029,11 +1029,13 @@ export class CalendarService extends BaseService {
         startday = moment(day).subtract(Math.floor(daysPerPage / 2), "days").format("YYYY/MM/DD");
         endday = moment(day).add(Math.floor(daysPerPage / 2), "days").format("YYYY/MM/DD");
         break;
-      case PageDirection.PageUp :
-        startday = moment(day).subtract(daysPerPage, "days").format("YYYY/MM/DD");
-        break;
       case PageDirection.PageDown :
-        endday = moment(day).add(daysPerPage, "days").format("YYYY/MM/DD");
+        endday = moment(day).subtract(1, "days").format("YYYY/MM/DD");
+        startday = moment(endday).subtract(daysPerPage, "days").format("YYYY/MM/DD");
+        break;
+      case PageDirection.PageUp :
+        startday = moment(day).add(1, "days").format("YYYY/MM/DD");
+        endday = moment(startday).add(daysPerPage, "days").format("YYYY/MM/DD");
         break;
       default:
         this.assertFail();
