df32e9af8058a8462f6d6e3ef83e10f58e38e96b
leon_xi <leon_xi@163.com>
2019-08-20 23:57:34 +0800

    更新测试case

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -264,9 +264,9 @@ describe('CalendarService test suite', () => {
     expect(activities.memos.length).toBe(0);
   });
 
-  xit(`Case 17 - 3 - 1 getCalendarActivities 取得日历画面显示活动一览 - 向下拉加载(未初始化报错)`, <any>fakeAsync(() => {
-    await expectAsync(calendarService.getCalendarActivities(PageDirection.PageDown)).toBeRejected();
-  }));
+  // xit(`Case 17 - 3 - 1 getCalendarActivities 取得日历画面显示活动一览 - 向下拉加载(未初始化报错)`, <any>fakeAsync(() => {
+  //   await expectAsync(calendarService.getCalendarActivities(PageDirection.PageDown)).toBeRejected();
+  // }));
 
   it(`Case 17 - 3 getCalendarActivities 取得日历画面显示活动一览 - 向下拉加载`, async () => {
     let month: string = moment().format("YYYY/MM");
@@ -285,9 +285,9 @@ describe('CalendarService test suite', () => {
     expect(calendarholdings[4].month).toBe(moment(month).add(2, "months").format("YYYY/MM"));
   });
 
-  xit(`Case 17 - 2 - 1 getCalendarActivities 取得日历画面显示活动一览 - 向上拉加载(未初始化报错)`, async () => {
-    await expectAsync(calendarService.getCalendarActivities(PageDirection.PageUp)).toBeRejected();
-  });
+  // xit(`Case 17 - 2 - 1 getCalendarActivities 取得日历画面显示活动一览 - 向上拉加载(未初始化报错)`, async () => {
+  //   await expectAsync(calendarService.getCalendarActivities(PageDirection.PageUp)).toBeRejected();
+  // });
 
   it(`Case 17 - 2 getCalendarActivities 取得日历画面显示活动一览 - 向上拉加载`, async () => {
     let month: string = moment().format("YYYY/MM");
