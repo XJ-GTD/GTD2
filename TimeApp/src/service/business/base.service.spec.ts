3704d63e527ca23c3cf8e7acef84509f97037d7c
leon_xi <leon_xi@163.com>
2019-08-23 18:30:13 +0800

    更新测试代码

--- TimeApp/src/service/business/base.service.spec.ts
+++ TimeApp/src/service/business/base.service.spec.ts
@@ -32,7 +32,7 @@ describe('BaseService test suite', () => {
 
   it('Case 7 - 3 assertFalse("4" == "5")', () => {
     expect(function() {
-      baseService.assertFalse("4" == "5");
+      baseService.assertFalse(("4" == "5"));
     }).toThrow();
   });
 
@@ -56,7 +56,7 @@ describe('BaseService test suite', () => {
 
   it('Case 6 - 3 assertTrue("4" == "5")', () => {
     expect(function() {
-      baseService.assertTrue("4" == "5");
+      baseService.assertTrue(("4" == "5"));
     }).not.toThrow();
   });
 
