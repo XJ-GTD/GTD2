0b66f2039fd49a3ea8a0b87f093c9d29fc90b4d7
leon_xi <leon_xi@163.com>
2019-08-24 21:38:21 +0800

    更新测试case

--- TimeApp/src/service/business/calendar.service.spec.ts
+++ TimeApp/src/service/business/calendar.service.spec.ts
@@ -2216,6 +2216,53 @@ describe('CalendarService test suite', () => {
     expect(dayActivities.memos.length).toBe(0);
   });
 
+  it(`Case 6 - 1 - 2 fetchDayActivities 取得指定日期的活动 - 当天有1个日历项、1个任务、1个备忘和1个小任务`, async () => {
+    let day: string = moment().format("YYYY/MM/DD");
+
+    // 日历项
+    let planitem1: PlanItemData = {} as PlanItemData;
+
+    planitem1.sd = day;
+    planitem1.jtn = "结婚纪念日";
+    planitem1.jtt = PlanItemType.Activity;
+
+    await calendarService.savePlanItem(planitem1);
+
+    // 任务
+    let task: TaskData = {} as TaskData;
+
+    task.evd = day;
+    task.evn = "结婚纪念日前给太太买礼物";
+
+    await eventService.saveTask(task);
+
+    // 备忘
+    let memo: MemoData = {} as MemoData;
+
+    memo.sd = day;
+    memo.mon = "结婚纪念日买了一块定制巧克力给太太, 太太很高兴";
+
+    await memoService.saveMemo(memo);
+
+    // 小任务
+    let minitask: MiniTaskData = {} as MiniTaskData;
+
+    minitask.evd = day;
+    minitask.evn = "结婚纪念日前给太太买礼物";
+
+    await eventService.saveMiniTask(minitask);
+
+    let dayActivities: DayActivityData = await calendarService.fetchDayActivities();
+
+    expect(dayActivities.day).toBe(day);
+    expect(dayActivities.calendaritems).toBeDefined();
+    expect(dayActivities.calendaritems.length).toBe(1);
+    expect(dayActivities.events).toBeDefined();
+    expect(dayActivities.events.length).toBe(2);
+    expect(dayActivities.memos).toBeDefined();
+    expect(dayActivities.memos.length).toBe(1);
+  });
+
   it(`Case 6 - 1 - 1 fetchDayActivities 取得指定日期的活动 - 当天有1个日历项、1个任务、1个备忘`, async () => {
     let day: string = moment().format("YYYY/MM/DD");
 
@@ -3200,6 +3247,23 @@ describe('CalendarService test suite', () => {
     expect(monthActivity.days.get(endday).events.length).toBe(0);
     expect(monthActivity.days.get(endday).memos).toBeDefined();
     expect(monthActivity.days.get(endday).memos.length).toBe(0);
+
+  });
+
+  it(`Case 3 - 5 fetchMonthActivities 取得当前月份活动 - 1个小任务`, async () => {
+    // 小任务
+    let minitask: MiniTaskData = {} as MiniTaskData;
+
+    minitask.evn = "结婚纪念日前给太太买礼物";
+
+    await eventService.saveMiniTask(minitask);
+
+    let monthActivity: MonthActivityData = await calendarService.fetchMonthActivities(moment().format("YYYY/MM"));
+
+    expect(monthActivity).toBeDefined();
+    expect(monthActivity.month).toBe(moment().format("YYYY/MM"));
+    expect(monthActivity.events).toBeDefined();
+    expect(monthActivity.events.length).toBeGreaterThan(0);
 
   });
 
