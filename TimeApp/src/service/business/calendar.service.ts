756d862c9302099b4afd32ae2761159abdd9a7b1
leon_xi <leon_xi@163.com>
2019-08-26 15:49:13 +0800

    更新代码

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -2327,7 +2327,10 @@ export class CalendarService extends BaseService {
 
     // 恢复备份参与人
     for (let par of pars) {
-      sqls.push(par.inTParam());
+      let pardb: ParTbl = new ParTbl();
+      Object.assign(pardb, par);
+
+      sqls.push(pardb.inTParam());
     }
 
     let fjs = recoveries.fj;
@@ -2337,7 +2340,10 @@ export class CalendarService extends BaseService {
 
     // 恢复备份附件
     for (let fj of fjs) {
-      sqls.push(fj.inTParam());
+      let fjdb: ParTbl = new ParTbl();
+      Object.assign(fjdb, fj);
+
+      sqls.push(fjdb.inTParam());
     }
 
     // 自动保存到数据库
