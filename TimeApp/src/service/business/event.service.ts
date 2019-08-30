7f4e247062612a08431695c3b29d4cc8c578e92d
leon_xi <leon_xi@163.com>
2019-08-30 11:46:36 +0800

    更新代码

--- TimeApp/src/service/business/event.service.ts
+++ TimeApp/src/service/business/event.service.ts
@@ -19,8 +19,7 @@ import {DataConfig} from "../config/data.config";
 import {BTbl} from "../sqlite/tbl/b.tbl";
 import {FjTbl} from "../sqlite/tbl/fj.tbl";
 import {DataRestful, PullInData, PushInData, SyncData} from "../restful/datasev";
-import {SyncType, DelType, SyncDataStatus} from "../../data.enum";
-import {SyncDataSecurity} from "../../data.enum";
+import {SyncType, DelType, SyncDataStatus, PageDirection, SyncDataSecurity} from "../../data.enum";
 
 @Injectable()
 export class EventService extends BaseService {
