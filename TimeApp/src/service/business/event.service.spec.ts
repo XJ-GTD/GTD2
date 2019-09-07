5b36fa440c39171f8c6b4bd7f8df7346956bf2cc
“ying <343253410@qq.com>
2019-09-07 11:11:05 +0800

    todolist 测试

--- TimeApp/src/service/business/event.service.spec.ts
+++ TimeApp/src/service/business/event.service.spec.ts
@@ -745,13 +745,13 @@ describe('EventService test suite', () => {
 
       expect(agenda1).toBeNull();
     });
-    
+
     it(`Case 4 - 1 todolist 是否加入todoList`, async () => {
-      
+
 			let agenda2: Array<AgendaData> = new Array<AgendaData>();
       agenda2 = await eventService.todolist();
 
-      expect(agenda2).toBeNull();
+      expect(agenda2).toBeDefined();
       expect(agenda2.length).toBeGreaterThan(0);
     });
   });
