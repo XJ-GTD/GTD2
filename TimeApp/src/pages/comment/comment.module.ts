3f823c130fdae898a8334cb47c84576475b55165
leon_xi <leon_xi@163.com>
2019-09-09 15:52:51 +0800

    更新代码

--- TimeApp/src/pages/comment/comment.module.ts
+++ TimeApp/src/pages/comment/comment.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommentPage } from './comment';
 import { IonicModule } from "ionic-angular";
 import { DirectivesModule } from "../../directives/directives.module";
+import {ModalBoxComponentModule} from "../../components/modal-box/modal-box.module";
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { DirectivesModule } from "../../directives/directives.module";
   ],
   imports: [
     IonicModule,
+    ModalBoxComponentModule,
     DirectivesModule
   ],
   providers: [
--- TimeApp/src/pages/comment/comment.ts
+++ TimeApp/src/pages/comment/comment.ts
@@ -1,28 +1,15 @@
 import { Component, ElementRef, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, ModalController, Scroll } from 'ionic-angular';
 import {Keyboard} from "@ionic-native/keyboard";
+import {ModalBoxComponent} from "../../components/modal-box/modal-box";
 
 @IonicPage()
 @Component({
   selector: 'page-comment',
   template: `
-  <ion-header no-border>
-    <ion-toolbar>
-      <ion-title>备注</ion-title>
-    </ion-toolbar>
-  </ion-header>
-
-  <ion-content>
+  <modal-box title="备注" onClose="close()">
     <ion-textarea type="text" placeholder="备注" [(ngModel)]="bz" class="memo-set" autosize maxHeight="400" #bzRef></ion-textarea>
-  </ion-content>
-
-  <ion-footer class="foot-set">
-    <ion-toolbar>
-    <button ion-button full (click)="close()">
-      关闭
-    </button>
-    </ion-toolbar>
-  </ion-footer>
+  </modal-box>
   `
 })
 export class CommentPage {
