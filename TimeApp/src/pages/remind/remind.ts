1d31d08095f5992f60e78455c583237821e5ef15
shopping <xuzp@linkstec.com>
2019-09-11 19:33:40 +0800

    提醒画面保存设置

--- TimeApp/src/pages/remind/remind.ts
+++ TimeApp/src/pages/remind/remind.ts
@@ -9,20 +9,11 @@ import {RtJson, TxJson} from "../../service/business/event.service";
   selector: 'page-remind',
   template: `
   <modal-box title="提醒" (onClose)="close()">
-    <scroll-select type="scroll-with-button" *ngFor="let remind of reminds" [value]="remind.value" (changed)="onRemindChanged($event)">
+    <scroll-select type="scroll-with-button" *ngFor="let remind of reminds"  [value]="remind.value" (changed)="onRemindChanged($event)">
       <scroll-select-option value="">滑动以添加</scroll-select-option>
-      <scroll-select-option value="5">5 分钟前</scroll-select-option>
-      <scroll-select-option value="10">10 分钟前</scroll-select-option>
-      <scroll-select-option value="15">15 分钟前</scroll-select-option>
-      <scroll-select-option value="30">30 分钟前</scroll-select-option>
-      <scroll-select-option value="60">1 小时前</scroll-select-option>
-      <scroll-select-option value="120">2 小时前</scroll-select-option>
-      <scroll-select-option value="180">3 小时前</scroll-select-option>
-      <scroll-select-option value="360">6 小时前</scroll-select-option>
-      <scroll-select-option value="720">12 小时前</scroll-select-option>
-      <scroll-select-option value="1440">1 天前</scroll-select-option>
-      <scroll-select-option value="2880">2 天前</scroll-select-option>
-      <scroll-select-option value="0">当事件开始</scroll-select-option>
+      <scroll-select-option [value]="opt.value" *ngFor="let opt of selectOption">
+        {{opt.caption}}
+      </scroll-select-option>
     </scroll-select>
   </modal-box>
   `
@@ -33,6 +24,7 @@ export class RemindPage {
   @ViewChildren(ScrollSelectComponent)
   remindComponents: QueryList<ScrollSelectComponent>;
 
+  selectOption : Array<any> = new Array<any>();
   reminds: Array<any> = new Array<any>();
   currentTx: TxJson;
 
@@ -41,15 +33,21 @@ export class RemindPage {
               public navParams: NavParams) {
     if (this.navParams && this.navParams.data) {
       let value = this.navParams.data.value;
+      //初始化选择项
+      let arrOption = [5, 10,15,30,60,120,180,360,720,1440,2880,0];
+      for (let j = 0,len  = arrOption.length; j < len ; j++){
+        this.selectOption.push({value : arrOption[j],caption : TxJson.caption(arrOption[j])});
+      }
 
       if (value) {
         this.currentTx = new TxJson();
         Object.assign(this.currentTx, value);
         if (this.currentTx.reminds.length > 0){
-          this.reminds.push({value:""});
+
           for (let j = 0, len = this.currentTx.reminds.length ; j < len ; j++){
             this.reminds.push({value:this.currentTx.reminds[j]});
           }
+          this.reminds.push({value:""});
         }else{
           this.reminds.push({value:""});
         }
@@ -63,17 +61,20 @@ export class RemindPage {
 
   close() {
     this.currentTx.reminds.length = 0;
-    for (let j = 0,len = this.reminds.length ; j < len ; j++){
-      this.currentTx.reminds.push(this.reminds[j].value);
-    }
+    this.remindComponents.forEach((value,index,array)=>{
+      if (value.value !=""){
+        this.currentTx.reminds.push(value.value);
+      }
+    })
+
     let data: Object = {txjson: this.currentTx};
     this.viewCtrl.dismiss(data);
   }
 
-  onRemindChanged(value) {
-    console.log(value);
+  onRemindChanged(value1) {
+    console.log(value1);
 
-    if (value) {
+    if (value1) {
       //选择或修改提醒
       let hasEmpty = this.remindComponents.some((value, index, array) => {
         return value.value == "";
