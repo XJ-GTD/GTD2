5e56ed17c8e857f45025ad447aa46e36c2b3e1a7
leon_xi <leon_xi@163.com>
2019-09-12 13:23:48 +0800

    更新代码

--- TimeApp/src/service/business/event.service.ts
+++ TimeApp/src/service/business/event.service.ts
@@ -173,7 +173,7 @@ export class EventService extends BaseService {
     if (!one || !another) return false;
 
     for (let key of Object.keys(one)) {
-      if (["wtt", "utt", "rts", "txs"].indexOf(key) >= 0) continue;   // 忽略字段
+      if (["wtt", "utt", "rts", "txs", "originator", "tos"].indexOf(key) >= 0) continue;   // 忽略字段
 
       if (one.hasOwnProperty(key)) {
         let value = one[key];
