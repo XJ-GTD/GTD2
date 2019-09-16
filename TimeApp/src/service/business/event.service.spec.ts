7c53d866a6b4e815dc224d027a6421d91f15433c
leon_xi <leon_xi@163.com>
2019-09-16 13:00:47 +0800

    更新测试代码

--- TimeApp/src/service/business/event.service.spec.ts
+++ TimeApp/src/service/business/event.service.spec.ts
@@ -689,7 +689,7 @@ describe('EventService test suite', () => {
   describe(`Case 20 - 1 取得两个日程变化的字段名成数组`, () => {
     let beforechange: AgendaData = {} as AgendaData;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
       beforechange.sd = moment().format("YYYY/MM/DD");
       beforechange.evn = "取得两个日程变化的字段名成数组 - 保存后比较";
 
@@ -715,6 +715,18 @@ describe('EventService test suite', () => {
       expect(changed).toBeDefined();
       expect(changed.length).toBeDefined(1);
       expect(changed[0]).toBeDefined("evn");
+    });
+
+    it(`Case 20 - 1 - 3 changedAgendaFields 取得两个日程变化的字段名成数组 - 修改备注后比较`, async () => {
+      let agenda: AgendaData = await eventService.getAgenda(beforechange.evi);
+
+      agenda.bz = "修改备注后";
+
+      let changed: Array<string> = eventService.changedAgendaFields(agenda, beforechange);
+
+      expect(changed).toBeDefined();
+      expect(changed.length).toBeDefined(1);
+      expect(changed[0]).toBeDefined("bz");
     });
   });
 
