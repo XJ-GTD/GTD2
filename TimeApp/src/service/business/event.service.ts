8d80f6a4a4d6558bc523f8c39ccb856bc85e1cd7
shopping <xuzp@linkstec.com>
2019-09-16 13:10:23 +0800

    只改备注，计划，TODOlist，提醒，则只更新ev表这四个字段

--- TimeApp/src/service/business/event.service.ts
+++ TimeApp/src/service/business/event.service.ts
@@ -1094,7 +1094,7 @@ export class EventService extends BaseService {
       ev.txs = newAgdata.txs
       ev.fj =newAgdata.fj;
       ev.pn = newAgdata.pn;
-      ev.wc = newAgenda.wc;
+      ev.wc = newAgdata.wc;
       await this.sqlExce.updateByParam(ev);
       outAgds.push(newAgdata);
       return outAgds;
