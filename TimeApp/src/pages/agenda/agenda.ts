6e58b1c88c0627991631abb9317b51c5d3cc691a
leon_xi <leon_xi@163.com>
2019-09-17 14:36:46 +0800

    更新

--- TimeApp/src/pages/agenda/agenda.ts
+++ TimeApp/src/pages/agenda/agenda.ts
@@ -15,7 +15,7 @@ import {PageBoxComponent} from "../../components/page-box/page-box";
 import {CornerBadgeComponent} from "../../components/corner-badge/corner-badge";
 import {CalendarService} from "../../service/business/calendar.service";
 import {EventService, AgendaData, RtJson, TxJson} from "../../service/business/event.service";
-import { PageDirection, IsSuccess, OperateType, RepeatFlag, ToDoListStatus } from "../../data.enum";
+import { PageDirection, IsSuccess, OperateType, RepeatFlag, ToDoListStatus, IsWholeday } from "../../data.enum";
 
 /**
  * Generated class for the 日程创建/修改 page.
