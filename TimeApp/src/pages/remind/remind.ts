5a14c6ca0b9edf751c9dacd8c343bba408d680c4
shopping <xuzp@linkstec.com>
2019-09-24 19:02:36 +0800

    提醒画面处理

--- TimeApp/src/pages/remind/remind.ts
+++ TimeApp/src/pages/remind/remind.ts
@@ -1,9 +1,11 @@
 import {Component, ElementRef, QueryList, Renderer2, ViewChild, ViewChildren} from '@angular/core';
-import {IonicPage, NavController, NavParams, ModalController, Scroll, ViewController} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ModalController, Scroll, ViewController, DateTime} from 'ionic-angular';
 import {ScrollSelectComponent} from "../../components/scroll-select/scroll-select";
 import {ModalBoxComponent} from "../../components/modal-box/modal-box";
 import {RtJson, TxJson} from "../../service/business/event.service";
 import {MultiPicker} from "ion-multi-picker";
+import * as moment from "moment";
+import * as anyenum from "../../data.enum";
 
 @IonicPage()
 @Component({
@@ -17,18 +19,42 @@ import {MultiPicker} from "ion-multi-picker";
       <!--</scroll-select-option>-->
       <!--</scroll-select>-->
       <ion-content>
-        <button (click)="openselect()" ion-button>ddddd</button>
+        <button (click)="openRemindTiqian()" ion-button>提前提醒</button>
+        <button (click)="openRemindDt()" ion-button>日期提醒</button>
         <ion-item>
-        <ion-multi-picker item-content #remindselect [multiPickerColumns]="dependentColumns"></ion-multi-picker>
+        <ion-multi-picker #remindTiqian [(ngModel)]="tiqianvalue" 
+                          (ngModelChange)="tiqianselect()" [multiPickerColumns]="dependentColumns"
+                          cancelText="取消" doneText="确认"></ion-multi-picker>
+          <div>
+            <ion-datetime #remindDt displayFormat="YYYY年MM月DD日 HH:mm"
+                          pickerFormat="YYYY MM DD HH mm" color="light"
+                          [(ngModel)]="dtvalue" (ngModelChange)="dtselect()"
+                          min="1999-01-01" max="2039-12-31" cancelText="取消" doneText="确认"
+            ></ion-datetime>
+          </div>
         </ion-item>
+        <ion-list no-lines  class="reind-list">
+          <ion-item class="plan-list-item"  *ngFor="let remind of reminds; let idx = index" >
+            <div class="color-dot"  item-start></div>
+            <ion-label>{{remind.datename}}</ion-label>
+            <button ion-button color="danger" (click)="delRemind(idx)" clear item-end>
+              <img class="content-gc" src="./assets/imgs/sc.png">
+            </button>
+          </ion-item>
+        </ion-list>
       </ion-content>
     </modal-box>
   `
 })
 export class RemindPage {
+
+  @ViewChild("remindTiqian")
+  remindTiqian:MultiPicker;
+
+  @ViewChild("remindDt")
+  remindDt:DateTime;
+
   statusBarColor: string = "#3c4d55";
-  @ViewChild("remindselect")
-  remindselect:MultiPicker;
 
   buttons: any = {
     remove: false,
@@ -37,15 +63,11 @@ export class RemindPage {
     cancel: true
   };
 
-  openselect(){
-    this.remindselect.open();
+  dtvalue : string;
+  tiqianvalue : string;
 
-  }
+  evdatetime ; string;
 
-  @ViewChildren(ScrollSelectComponent)
-  remindComponents: QueryList<ScrollSelectComponent>;
-
-  selectOption: Array<any> = new Array<any>();
   reminds: Array<any> = new Array<any>();
   currentTx: TxJson;
   dependentColumns: any[];
@@ -64,130 +86,163 @@ export class RemindPage {
       {
         columnWidth: '100px',
         options: [
-          {text: '1天', value: '1d'},
-          {text: '2天', value: '2d'},
-          {text: '3天', value: '3d'},
-          {text: '4天', value: '4d'},
-          {text: '5天', value: '5d'},
-          {text: '6天', value: '6d'},
-          {text: '7天', value: '7d'},
-          {text: '8天', value: '8d'},
-          {text: '9天', value: '9d'},
-          {text: '10天', value: '10d'}],
+          {text: '1天', value: this.day2min(1)},
+          {text: '2天', value: this.day2min(2)},
+          {text: '3天', value: this.day2min(3)},
+          {text: '4天', value: this.day2min(4)},
+          {text: '5天', value: this.day2min(5)},
+          {text: '6天', value: this.day2min(6)},
+          {text: '7天', value: this.day2min(7)},
+          {text: '8天', value: this.day2min(8)},
+          {text: '9天', value: this.day2min(9)},
+          {text: '10天', value: this.day2min(10)}],
       },
       {
         columnWidth: '100px',
         options: [
-          {text: '1小时', value: '1h'},
-          {text: '2小时', value: '2h'},
-          {text: '3小时', value: '3h'},
-          {text: '4小时', value: '4h'},
-          {text: '5小时', value: '5h'},
-          {text: '6小时', value: '6h'},
-          {text: '7小时', value: '7h'},
-          {text: '8小时', value: '8h'},
-          {text: '9小时', value: '9h'},
-          {text: '10小时', value: '10h'},
-          {text: '11小时', value: '11h'},
-          {text: '12小时', value: '12h'},
-          {text: '13小时', value: '13h'},
-          {text: '14小时', value: '14h'},
-          {text: '15小时', value: '15h'},
-          {text: '16小时', value: '16h'}],
+          {text: '', value: this.h2min(0)},
+          {text: '1小时', value: this.h2min(1)},
+          {text: '2小时', value: this.h2min(2)},
+          {text: '3小时', value: this.h2min(3)},
+          {text: '4小时', value: this.h2min(4)},
+          {text: '5小时', value: this.h2min(5)},
+          {text: '6小时', value: this.h2min(6)},
+          {text: '7小时', value: this.h2min(7)},
+          {text: '8小时', value: this.h2min(8)},
+          {text: '9小时', value: this.h2min(9)},
+          {text: '10小时', value:this.h2min(10)},
+          {text: '11小时', value:this.h2min(11)},
+          {text: '12小时', value:this.h2min(12)},
+          {text: '13小时', value:this.h2min(13)},
+          {text: '14小时', value:this.h2min(14)},
+          {text: '15小时', value:this.h2min(15)},
+          {text: '16小时', value:this.h2min(16)}],
       },
       {
         columnWidth: '100px',
         options:
           [
-            {text: '5分钟', value: '5m'},
-            {text: '10分钟', value: '10m'},
-            {text: '15分钟', value: '15m'},
-            {text: '20分钟', value: '20m'},
-            {text: '25分钟', value: '25m'},
-            {text: '30分钟', value: '30m'},
-            {text: '35分钟', value: '35m'},
-            {text: '40分钟', value: '40m'},
-            {text: '45分钟', value: '45m'},
-            {text: '50分钟', value: '50m'},
-            {text: '55分钟', value: '55m'}
+            {text: '', value: 0},
+            {text: '5分钟', value: 5},
+            {text: '10分钟', value: 10},
+            {text: '15分钟', value: 15},
+            {text: '20分钟', value: 20},
+            {text: '25分钟', value: 25},
+            {text: '30分钟', value: 30},
+            {text: '35分钟', value: 35},
+            {text: '40分钟', value: 40},
+            {text: '45分钟', value: 45},
+            {text: '50分钟', value: 50},
+            {text: '55分钟', value: 55}
           ]
       }
     ];
 
     if (this.navParams && this.navParams.data) {
       let value = this.navParams.data.value;
-      //初始化选择项
-      let arrOption = [5, 10, 15, 30, 60, 120, 180, 360, 720, 1440, 2880, 0];
-      for (let j = 0, len = arrOption.length; j < len; j++) {
-        this.selectOption.push({value: arrOption[j], caption: TxJson.caption(arrOption[j])});
-      }
 
       if (value) {
         this.currentTx = new TxJson();
-        Object.assign(this.currentTx, value);
+        Object.assign(this.currentTx, value.txjson);
         if (this.currentTx.reminds.length > 0) {
 
-          for (let j = 0, len = this.currentTx.reminds.length; j < len; j++) {
-            this.reminds.push({value: this.currentTx.reminds[j]});
+          if (value.al == anyenum.IsWholeday.NonWhole) {
+            this.evdatetime = moment(value.evd + " " + value.evt).format("YYYY/MM/DD HH:mm");
+
+          } else {
+            this.evdatetime = moment(value.evd + " " + "08:00").format("YYYY/MM/DD HH:mm");
+
           }
-          this.reminds.push({value: ""});
-        } else {
-          this.reminds.push({value: ""});
+          for (let j = 0, len = this.currentTx.reminds.length; j < len; j++) {
+            this.reminds.push(
+              {
+                datename:this.getShowDateName(this.currentTx.reminds[j]) ,
+                value: this.currentTx.reminds[j]
+              });
+          }
         }
+
       }
+
     }
-  }
-
-  async ionViewWillEnter() {
-
   }
 
   save() {
     this.currentTx.reminds.length = 0;
-    this.remindComponents.forEach((value, index, array) => {
-      if (value.value != "") {
-        this.currentTx.reminds.push(value.value);
-      }
-    })
+    for (let j = 0,len = this.reminds.length; j < len ; j++){
+        this.currentTx.reminds.push(this.reminds[j].value);
+    }
 
     let data: Object = {txjson: this.currentTx};
     this.viewCtrl.dismiss(data);
+  }
+
+  getShowDateName(time){
+    let ret : string;
+    ret = moment(this.evdatetime).subtract(time, 'm').format("YYYY/MM/DD HH:mm");
+
+    ret = ret + "(" +TxJson.caption(time) + ")";
+
+    return ret;
   }
 
   cancel() {
     this.navCtrl.pop();
   }
 
-  onRemindChanged(value1) {
-    console.log(value1);
+  openRemindTiqian(){
+    this.remindTiqian.open();
 
-    if (value1) {
-      //选择或修改提醒
-      let hasEmpty = this.remindComponents.some((value, index, array) => {
-        return value.value == "";
-      });
+  }
+  openRemindDt(){
+    this.remindDt.open();
 
-      if (!hasEmpty) {
-        this.reminds.push({value: ""});
-      }
-    } else {
-      //取消提醒
-      let forRemoved: Array<number> = new Array<number>();
+  }
 
-      this.remindComponents.forEach((item, index) => {
-        if (item.value == "") forRemoved.push(index);
-      });
+  tiqianselect(){
 
-      let results: Array<any> = this.reminds.filter((element, index) => {
-        return forRemoved.indexOf(index) < 0;
-      });
-
-      if (!results)
-        results = new Array<any>();
-
-      results.push({value: ""});
-
-      this.reminds = results;
+    if (!this.tiqianvalue){
+      return;
     }
+    let dtsplit = new Array<string>();
+    let time;
+    let dt;
+    dtsplit = this.tiqianvalue.split(" ");
+    if (dtsplit.length < 4){
+      return;
+    }
+
+    time =  parseInt(dtsplit[1]) + parseInt(dtsplit[2]) + parseInt(dtsplit[3]);
+    dt = moment(this.evdatetime).subtract(time,'m');
+    this.reminds.push(
+      {
+        datename:moment(this.evdatetime).subtract(time, 'm').format("YYYY/MM/DD HH:mm")
+        + "(" +TxJson.caption(time) + ")",
+        value:time
+      });
+  }
+
+  dtselect(){
+
+    let dt = this.dtvalue.replace("T"," ");
+    dt = dt.replace("Z","");
+    let time = moment(this.evdatetime).diff(dt,'m');
+    this.reminds.push(
+      {
+        datename:moment(dt).format("YYYY/MM/DD HH:mm") + "(" +TxJson.caption(time) + ")",
+        value:time
+      });
+  }
+
+  day2min(d){
+    return d * 24 * 60;
+  }
+
+  h2min(h){
+    return h * 60;
+  }
+
+  delRemind(index){
+    this.reminds.splice(index,1);
   }
 }
