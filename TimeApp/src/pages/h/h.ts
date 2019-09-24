a606a470c981830bb3cd77910cf05c0e0b690e0f
leon_xi <leon_xi@163.com>
2019-09-24 11:17:11 +0800

    更新代码

--- TimeApp/src/pages/h/h.ts
+++ TimeApp/src/pages/h/h.ts
@@ -388,7 +388,7 @@ export class HPage {
     this.modalCtr.create(DataConfig.PAGE._DO_PAGE, selectDay).present();
   }
 
-  todoscrumList() {
+  todoscrumList(day ?: CalendarDay) {
     let selectDay: CalendarDay = day ? day : this.hdata.selectDay;
 
     this.modalCtr.create(DataConfig.PAGE._DOSCRUM_PAGE, selectDay).present();
