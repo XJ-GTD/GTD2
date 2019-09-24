b8e3ab81da7191b51dfece57fe168e011386c379
leon_xi <leon_xi@163.com>
2019-09-24 18:32:59 +0800

    更新

--- TimeApp/src/service/business/effect.service.ts
+++ TimeApp/src/service/business/effect.service.ts
@@ -13,13 +13,15 @@ import {
   assertNotNull,
   assertFail
 } from "../../util/util";
+import { SqliteExec } from "../util-service/sqlite.exec";
+import { UtilService } from "../util-service/util.service";
+import { UserConfig } from "../config/user.config";
 
 @Injectable()
 export class EffectService extends BaseService {
   constructor(private sqlExce: SqliteExec,
               private util: UtilService,
-              private userConfig: UserConfig,
-              private dataRestful: DataRestful) {
+              private userConfig: UserConfig) {
     super();
   }
 }
