cebf691cfe56bc99fda5f2de810bc2ab8cd7130e
leon_xi <leon_xi@163.com>
2019-09-30 11:00:15 +0800

    更新代码

--- TimeApp/src/service/business/event.service.ts
+++ TimeApp/src/service/business/event.service.ts
@@ -2418,6 +2418,7 @@ export class EventService extends BaseService {
         sync.id = agenda.evi;
         sync.type = "Agenda";
         sync.title = agenda.evn;
+        sync.datetime = agenda.evd + " " + agenda.evt;
 
         // 非重复日程/重复日程的第一条需要通知
         if (!agenda.rtevi || agenda.rfg == RepeatFlag.RepeatToOnly) {
