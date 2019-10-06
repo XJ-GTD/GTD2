35b656af556d6f4f9cce5dbfd81f8b82aacb5bda
leon_xi <leon_xi@163.com>
2019-10-06 11:34:37 +0800

    更新代码

--- TimeApp/src/pages/tdl/tdl.ts
+++ TimeApp/src/pages/tdl/tdl.ts
@@ -124,7 +124,7 @@ import {UserConfig} from "../../service/config/user.config";
                       <ion-icon class = "user-o fal fa-user-tag"></ion-icon>
                     </div>
                     <div class="person">--来自{{event.ui | formatuser: currentuser: friends}}</div>
-                    <div class="invite" *ngIf="event.invitestatus != inviteaccept || event.invitestatus != invitereject" end><span (click)="acceptInvite($event, event)">接受</span><span (click)="rejectInvite($event, event)">拒绝</span></div>
+                    <div class="invite" *ngIf="event.invitestatus != inviteaccept && event.invitestatus != invitereject" end><span (click)="acceptInvite($event, event)">接受</span><span (click)="rejectInvite($event, event)">拒绝</span></div>
                   </div>
               </ion-row>
 
