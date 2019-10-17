175edecdd6d7c9f3c2196943e79e58b13a551643
leon_xi <leon_xi@163.com>
2019-10-17 16:13:59 +0800

    更新代码

--- TimeApp/src/pages/do/do.module.ts
+++ TimeApp/src/pages/do/do.module.ts
@@ -4,12 +4,14 @@ import { DoPage } from "./do";
 import { DoService } from "./do.service";
 import {TaskListComponentModule} from "../../components/task-list/task-list.module";
 import {PageBoxComponentModule} from "../../components/page-box/page-box.module";
+import {PipesModule} from "../../pipes/pipes.module";
 
 @NgModule({
   declarations: [
     DoPage,
   ],
   imports: [
+    PipesModule,
     IonicPageModule,
     TaskListComponentModule,
     PageBoxComponentModule
