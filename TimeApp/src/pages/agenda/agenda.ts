e972c30eac03d5d188c3aca8c721841137107dfe
zhangjy <jy-zhang@zakj.info>
2019-11-07 14:28:07 +0800

    Merge branch 'cassiscornuta' of github.com:XJ-GTD/GTD2 into cassiscornuta
    # Conflicts:
    #	TimeApp/src/pages/attach/attach.ts

--- TimeApp/src/pages/agenda/agenda.ts
+++ TimeApp/src/pages/agenda/agenda.ts
@@ -80,6 +80,10 @@ import {ModiPower} from "../../data.enum";
                       [class.fad]="currentAgenda.todolist == todoliston"
                       [class.fal]="currentAgenda.todolist != todoliston"></ion-icon>
 
+            <ion-icon class="fad fa-ear font-large-x "></ion-icon>
+
+            <ion-icon class="fad fa-microphone font-large-x"></ion-icon>
+
           </div>
         </ion-row>
 
@@ -826,6 +830,8 @@ export class AgendaPage {
   }
 
   speaker() {
+
+
   }
 
   openfriend(ui) {
