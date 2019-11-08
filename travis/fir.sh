b3496c83e928b098a30ac0829b6e259ea51c88de
leon_xi <leon_xi@163.com>
2019-11-08 22:16:39 +0800

    苹果版本自动更新

--- travis/fir.sh
+++ travis/fir.sh
@@ -2,7 +2,6 @@
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     # fir on macOS
-    #fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/ios/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
     ls -la $TRAVIS_BUILD_DIR/build/debug
     cat $TRAVIS_BUILD_DIR/build/debug/DistributionSummary.plist
     cat $TRAVIS_BUILD_DIR/build/debug/ExportOptions.plist
@@ -11,9 +10,12 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     cp $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa
     if [ $TRAVIS_BRANCH = 'cassiscornuta' ]; then
       if [ -f "$TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa" ]; then
+        echo "upload to appstore"
         alias altool='/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool'
         source ~/.bash_profile
         altool --validate-app -f $TRAVIS_BUILD_DIR/build/debug/$IOS_APP_NAME.ipa -t ios -u $APPSTORE_USERNAME -p $APPSTORE_PASSWORD
+
+        echo "upload to fir.im"
         fir publish $TRAVIS_BUILD_DIR/build/debug/app-$TRAVIS_BRANCH.ipa -T $FIR_TOKEN_CASSISCORNUTA -c "唐冠螺: ${TRAVIS_COMMIT_MESSAGE}"
       fi
     else
@@ -21,7 +23,6 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     fi
 else
     # fir on Linux
-    #fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk -T $FIR_TOKEN
     cp $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk
     if [ $TRAVIS_BRANCH = 'cassiscornuta' ]; then
       fir p $TRAVIS_BUILD_DIR/TimeApp/platforms/android/app/build/outputs/apk/debug/app-$TRAVIS_BRANCH.apk -T $FIR_TOKEN_CASSISCORNUTA -c "唐冠螺: ${TRAVIS_COMMIT_MESSAGE}"
