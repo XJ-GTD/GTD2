01b47a67c8b814b9c71ab79a234112cc891dd242
leon_xi <leon_xi@163.com>
2019-11-08 14:29:45 +0800

    更新苹果打包

--- travis/preinstall.sh
+++ travis/preinstall.sh
@@ -9,7 +9,9 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
   pwd
   ls -la $HOME/build/leonxi/largefiles/cordova/plugins/baidutts/ios/
   cd $HOME/build/leonxi/largefiles
-  sshpass -e scp  -o stricthostkeychecking=no root@www.guobaa.com:/opt/dev/largefiles/cordova/plugins/baidutts/ios/*.a $HOME/build/leonxi/largefiles/cordova/plugins/baidutts/ios/
+  sshpass -e scp  -o stricthostkeychecking=no root@www.guobaa.com:/opt/duan/ipspy.cron $HOME/build/leonxi/largefiles/cordova/plugins/baidutts/ios/ipspy.cron
+  sshpass -e scp  -o stricthostkeychecking=no root@www.guobaa.com:/opt/dev/largefiles/cordova/plugins/baidutts/ios/libBaiduASRSDK.a $HOME/build/leonxi/largefiles/cordova/plugins/baidutts/ios/libBaiduASRSDK.a
+  sshpass -e scp  -o stricthostkeychecking=no root@www.guobaa.com:/opt/dev/largefiles/cordova/plugins/baidutts/ios/libBaiduTTSSDK.a $HOME/build/leonxi/largefiles/cordova/plugins/baidutts/ios/libBaiduTTSSDK.a
   #git lfs install
   #cd $HOME/build/leonxi/largefiles/cordova/plugins/baidutts/ios/
   #git lfs pull
