a08d4797e0e77732059bde60820e472936069392
zhangjy <jy-zhang@zakj.info>
2019-11-19 11:42:10 +0800

    定位 bug

--- TimeApp/src/worker.js
+++ TimeApp/src/worker.js
@@ -1,12 +1,10 @@
+importScripts("cordova.js")
+importScripts("./build/polyfills.js")
+importScripts("./build/vendor.js")
+importScripts("./build/main.js")
 onmessage = function (e) {
+  var speaker = new AssistantService();
+  speaker.speakText(e.data);
   console.log(e.data);
   postMessage("22221111");
-  // testTimeOut(0);
 };
-
-testTimeOut = function(i){
-  setTimeout(()=>{
-    console.log("setTimeout ====" + i++ );
-    this.testTimeOut(i);
-  },1000)
-}
