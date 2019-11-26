125d250107c15e1f0e45f1ac0f7c5245c0cc6754
leon_xi <leon_xi@163.com>
2019-11-26 11:51:35 +0800

    更新脚本

--- travis/preinstall.sh
+++ travis/preinstall.sh
@@ -31,7 +31,7 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 else
   yarn global add ionic@4.1.2 phonegap@^8.0.0 plugman cordova@6.5.0
 
-  if [$TRAVIS_JOB_NAME = 'androidpackage']; then
+  if [ $TRAVIS_JOB_NAME = 'androidpackage' ]; then
     mkdir -p $ANDROID_HOME/licenses
     echo -e "d56f5187479451eabf01fb78af6dfcb131a6481e" >> $ANDROID_HOME/licenses/android-sdk-license
     echo -e "84831b9409646a918e30573bab4c9c91346d8abd" >> $ANDROID_HOME/licenses/android-sdk-preview-license
