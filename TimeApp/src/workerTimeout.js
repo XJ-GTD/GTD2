fd9fbb369fcaeaa38621977f7c5d7c31fcd047a2
zhangjy <jy-zhang@zakj.info>
2019-11-27 15:31:12 +0800

    同步Q不要timeout测试修改

--- TimeApp/src/workerTimeout.js
+++ TimeApp/src/workerTimeout.js
@@ -4,7 +4,6 @@ onmessage = function (e) {
   if (!timeObj){
     let time = e.data ? e.data : 0;
     timeObj = setTimeout(() => {
-      console.log("当前任务=====测试timeout是否存活");
       clearTimeout(timeObj);
       timeObj = null;
       postMessage("");
