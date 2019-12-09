0b4a398b29d680c3d2c5d38175a874b278cf125a
leon_xi <leon_xi@163.com>
2019-12-09 15:48:59 +0800

    At读写问题修正

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -3334,7 +3334,7 @@ export class CalendarService extends BaseService {
           // 不存在写入数据, 直接设置已读
           this.annotation(annotation.obi, false);
         } else {
-          if ((writeOriginData.nval || writeOriginData.cval || writeOriginData.bval || writeOriginData.checksum) == (readNewData.nval || readNewData.cval || readNewData.bval || readNewData.checksum)) {
+          if ((writeOriginData.nval || writeOriginData.cval || writeOriginData.bval || writeOriginData.checksum) && (readNewData.nval || readNewData.cval || readNewData.bval || readNewData.checksum)) {
             // 读取数据和写入数据一致
             this.annotation(annotation.obi, false);
           } else {
@@ -3511,7 +3511,7 @@ export class CalendarService extends BaseService {
           // 不存在读取数据, 直接设置未读
           this.annotation(annotation.obi, true);
         } else {
-          if ((readOriginData.nval || readOriginData.cval || readOriginData.bval || readOriginData.checksum) == (writeNewData.nval || writeNewData.cval || writeNewData.bval || writeNewData.checksum)) {
+          if ((readOriginData.nval || readOriginData.cval || readOriginData.bval || readOriginData.checksum) && (writeNewData.nval || writeNewData.cval || writeNewData.bval || writeNewData.checksum)) {
             // 读取数据和写入数据一致
             this.annotation(annotation.obi, false);
           } else {
