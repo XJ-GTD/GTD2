7554e511b4367e8ced4a4ccff0e521449a380181
leon_xi <leon_xi@163.com>
2019-12-09 15:21:42 +0800

    附件读取问题调试

--- TimeApp/src/service/business/calendar.service.ts
+++ TimeApp/src/service/business/calendar.service.ts
@@ -3263,12 +3263,12 @@ export class CalendarService extends BaseService {
           // 不存在写入数据, 直接设置已读
           this.commit(attachment.obi, false);
         } else {
-          console.log(`read ${readNewData.nval || readNewData.cval || readNewData.bval || readNewData.checksum}`);
-          console.log(`write ${writeOriginData.nval || writeOriginData.cval || writeOriginData.bval || writeOriginData.checksum}`);
           if ((writeOriginData.nval || writeOriginData.cval || writeOriginData.bval || writeOriginData.checksum) == (readNewData.nval || readNewData.cval || readNewData.bval || readNewData.checksum)) {
             // 读取数据和写入数据一致
             this.commit(attachment.obi, false);
           } else {
+            console.log(`read ${readNewData.nval || readNewData.cval || readNewData.bval || readNewData.checksum}`);
+            console.log(`write ${writeOriginData.nval || writeOriginData.cval || writeOriginData.bval || writeOriginData.checksum}`);
             // 读取数据和写入数据不一致
             this.commit(attachment.obi, true);
           }
