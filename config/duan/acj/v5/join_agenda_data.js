210fc3ab4b8d5407ff8b840b0f5b37c495ef9542
leon_xi <leon_xi@163.com>
2019-12-10 13:45:57 +0800

    增加分享加入处理流和日程数据加入数据转换

---
+++ config/duan/acj/v5/join_agenda_data.js
@@ -0,0 +1,60 @@
+//MWXING_JOIN_AGENDA_V1_5
+function shouldclean(datasource) {
+  var result = {};
+  // filter source code here start
+  var input = JSON.parse(datasource);
+
+  if (input !== undefined && input['userinfo'] && input['deviceId'] && input['deviceId'].startsWith('web_') && input['datas']) {
+
+    return true;
+  }
+
+  // filter source code here end
+  return false;
+}
+
+/**
+ * 获取原有数据，添加参与人后生成新的push数据
+ **/
+function clean(datasource) {
+  var result = {};
+  print('Start Nashorn Javascript processing...');
+  print(datasource);
+  // filter source code here start
+  var input = JSON.parse(datasource);
+
+  var userinfo = input['userinfo'];
+  var deviceId = input['deviceId'];
+  var datas = input['datas'];
+
+  // 转换成mwxing_data_sync_push_start处理流输入参数格式
+  var output = {
+    mwxing: {
+      lt: '',
+      pi: '',
+      pv: '',
+      dt: 'thirdparty',
+      ai: userinfo.openid,
+      di: deviceId
+    },
+    mpn: userinfo.phoneno,
+    name: userinfo.nickname,
+    d: []
+  };
+
+  for (var index in datas) {
+    var data = datas[index];
+
+    if (!data.to) data.to = [];
+    // 参与人不存在本次加入用户，则加入
+    if (data.to.indexOf(userinfo.phoneno) < 0) {
+      data.to.push(userinfo.phoneno);
+    } else {
+      continue;
+    }
+    // checksum 等待日程checksum处理逻辑后补充
+    output.d.push(data);
+  }
+
+  return JSON.stringify(output);
+}
