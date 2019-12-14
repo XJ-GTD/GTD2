fad72dc9372a7827d7ccde1c0a9b67250d1b7169
leon_xi <leon_xi@163.com>
2019-12-14 23:27:56 +0800

    设置小任务提醒

--- TimeApp/src/ws/process/remind.process.ts
+++ TimeApp/src/ws/process/remind.process.ts
@@ -63,7 +63,7 @@ export class RemindProcess extends BaseProcess implements MQProcess {
       minitask.evt = rdData.t;
 
       let txjson: TxJson = new TxJson();
-      txjson.reminds.push(Number(moment(minitask.evd + " " + minitask.evt, "YYYY/MM/DD HH:mm").format("YYYYMMDDHHmm")));
+      txjson.reminds.push(Number(moment(minitask.evd + " " + minitask.evt, "YYYY/MM/DD HH:mm").format("YYYYMMDDHHmm")) * -1);
 
       minitask.txjson = txjson;
 
@@ -94,7 +94,7 @@ export class RemindProcess extends BaseProcess implements MQProcess {
       minitask.evt = moment().format("HH:mm");
 
       let txjson: TxJson = new TxJson();
-      txjson.reminds.push(Number(rightnow.format("YYYYMMDDHHmm")));
+      txjson.reminds.push(Number(rightnow.format("YYYYMMDDHHmm")) * -1);
 
       minitask.txjson = txjson;
 
