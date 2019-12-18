805f9ee919c44f95aa84ea63eeecb257fa7645c2
leon_xi <leon_xi@163.com>
2019-12-18 15:19:40 +0800

    增加上下文参数

--- TimeApp/src/ws/process/choose.process.ts
+++ TimeApp/src/ws/process/choose.process.ts
@@ -32,6 +32,22 @@ export class ChooseProcess extends BaseProcess implements MQProcess{
     let paused: Array<any> = new Array<any>();
     paused = this.input(content, contextRetMap, "paused", WsDataConfig.PAUSED, paused) || paused;
 
+    //上下文内获取日程查询结果
+    let scd:Array<ScdData> = new Array<ScdData>();
+    scd = this.input(content,contextRetMap,"agendas",WsDataConfig.SCD,scd);
+
+    //上下文内获取日程查询结果
+    let memos:Array<ScdData> = new Array<ScdData>();
+    memos = this.input(content,contextRetMap,"memos",WsDataConfig.MOD,memos);
+
+    //上下文内获取日程查询结果
+    let planitems:Array<ScdData> = new Array<ScdData>();
+    planitems = this.input(content,contextRetMap,"planitems",WsDataConfig.PID,planitems);
+
+    //上下文内获取查询条件用日程人员或创建的日程人员
+    let fs :Array<FsData> = new Array<FsData>();
+    fs = this.input(content,contextRetMap,"contacts",WsDataConfig.FS,fs);
+
     //process处理符合条件则执行
     if (content.when && content.when !=""){
       let rf :boolean = false;
