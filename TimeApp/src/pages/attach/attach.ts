d0a57f308e0cb05bde877e6ff0a7d343c0a8c723
leon_xi <leon_xi@163.com>
2020-01-08 14:50:14 +0800

    增加调试

--- TimeApp/src/pages/attach/attach.ts
+++ TimeApp/src/pages/attach/attach.ts
@@ -280,37 +280,37 @@ export class AttachPage {
       console.info("开始拍照上传照片");
       alert(imageData);
       alert(JSON.stringify(imageData));
-      if (imageData != '') {
+      this.file.resolveNativePath(imageData)
+      .then((imagepath) => {
+        alert(imagepath);
+        alert(JSON.stringify(imagepath));
+        if (imagepath != '') {
 
-        let fileName: string = imageData.substr(imageData.lastIndexOf("/") + 1, imageData.length);
-        let ext: string = fileName.substr(fileName.lastIndexOf(".") + 1);
-        //将文件copy至缓存文件
-        let imgFileDir: string = imageData.substr(0, imageData.lastIndexOf("/") + 1);
-        let newFileName = this.util.getUuid() + "." + ext;
-        this.fjData.obt = this.obt;
-        this.fjData.obi = this.obi;
-        //this.fjData.fjn = newFileName;
-        this.fjData.ext = ext;
-        //构造地址文件
-        let cacheFilePathJson: CacheFilePathJson = new CacheFilePathJson();
-        cacheFilePathJson.local = "/" + newFileName;
-        this.fjData.fj = JSON.stringify(cacheFilePathJson);
-        this.fjData.fpjson = cacheFilePathJson;
-        this.fjData.fjurl = this.fjData.fpjson.getLocalFilePath(this.file.dataDirectory);
-        this.fjData.ui = this.currentuser;
-        this.fjData.members = this.members;
-        // if(!this.bw) {
-        //   this.bw = fileName;
-        // }
-        this.file.copyFile(imgFileDir, fileName, this.file.dataDirectory + cacheFilePathJson.getCacheDir(), newFileName).then(_ => {
-          this.saveFile();
-        });
-      }
-      // let base64Image = 'data:image/jpeg;base64,' + imageData;
-      // this.imgUrl = base64Image;
-      // alert(this.imgUrl);
-      //TODO  调研图片上传的Service,并将附件的返回值返回给上一页
-      //this.upload();
+          let fileName: string = imagepath.substr(imagepath.lastIndexOf("/") + 1, imagepath.length);
+          let ext: string = fileName.substr(fileName.lastIndexOf(".") + 1);
+          //将文件copy至缓存文件
+          let imgFileDir: string = imagepath.substr(0, imagepath.lastIndexOf("/") + 1);
+          let newFileName = this.util.getUuid() + "." + ext;
+          this.fjData.obt = this.obt;
+          this.fjData.obi = this.obi;
+          //this.fjData.fjn = newFileName;
+          this.fjData.ext = ext;
+          //构造地址文件
+          let cacheFilePathJson: CacheFilePathJson = new CacheFilePathJson();
+          cacheFilePathJson.local = "/" + newFileName;
+          this.fjData.fj = JSON.stringify(cacheFilePathJson);
+          this.fjData.fpjson = cacheFilePathJson;
+          this.fjData.fjurl = this.fjData.fpjson.getLocalFilePath(this.file.dataDirectory);
+          this.fjData.ui = this.currentuser;
+          this.fjData.members = this.members;
+          // if(!this.bw) {
+          //   this.bw = fileName;
+          // }
+          this.file.copyFile(imgFileDir, fileName, this.file.dataDirectory + cacheFilePathJson.getCacheDir(), newFileName).then(_ => {
+            this.saveFile();
+          });
+        }
+      });
     }, (err) => {
       console.info("拍照上传附件异常，异常信息：" + err);
     });
@@ -331,37 +331,36 @@ export class AttachPage {
       console.info("开始拍照上传照片");
       alert(imageData);
       alert(JSON.stringify(imageData));
-      if (imageData != '') {
+      this.file.resolveNativePath(imageData)
+      .then((imagepath) => {
+        alert(imagepath);
+        alert(JSON.stringify(imagepath));
+        if (imagepath != '') {
 
-        let fileName: string = imageData.substr(imageData.lastIndexOf("/") + 1, imageData.length);
-        let ext: string = fileName.substr(fileName.lastIndexOf(".") + 1);
-        //将文件copy至缓存文件
-        let imgFileDir: string = imageData.substr(0, imageData.lastIndexOf("/") + 1);
-        let newFileName = this.util.getUuid() + "." + ext;
-        this.fjData.obt = this.obt;
-        this.fjData.obi = this.obi;
-        //this.fjData.fjn = newFileName;
-        this.fjData.ext = ext;
-        //构造地址文件
-        let cacheFilePathJson: CacheFilePathJson = new CacheFilePathJson();
-        cacheFilePathJson.local = "/" + newFileName;
-        this.fjData.fj = JSON.stringify(cacheFilePathJson);
-        this.fjData.fpjson = cacheFilePathJson;
-        this.fjData.fjurl = this.fjData.fpjson.getLocalFilePath(this.file.dataDirectory);
-        this.fjData.ui = this.currentuser;
-        this.fjData.members = this.members;
-        // if(!this.bw) {
-        //   this.bw = fileName;
-        // }
-        this.file.copyFile(imgFileDir, fileName, this.file.dataDirectory + cacheFilePathJson.getCacheDir(), newFileName).then(_ => {
-          this.saveFile();
-        });
-      }
-      // let base64Image = 'data:image/jpeg;base64,' + imageData;
-      // this.imgUrl = base64Image;
-      // alert(this.imgUrl);
-      //TODO  调研图片上传的Service,并将附件的返回值返回给上一页
-      //this.upload();
+          let fileName: string = imagepath.substr(imagepath.lastIndexOf("/") + 1, imagepath.length);
+          let ext: string = fileName.substr(fileName.lastIndexOf(".") + 1);
+
+          //将文件copy至缓存文件
+          let imgFileDir: string = imagepath.substr(0, imagepath.lastIndexOf("/") + 1);
+          let newFileName = this.util.getUuid() + "." + ext;
+          this.fjData.obt = this.obt;
+          this.fjData.obi = this.obi;
+          this.fjData.ext = ext;
+
+          //构造地址文件
+          let cacheFilePathJson: CacheFilePathJson = new CacheFilePathJson();
+          cacheFilePathJson.local = "/" + newFileName;
+          this.fjData.fj = JSON.stringify(cacheFilePathJson);
+          this.fjData.fpjson = cacheFilePathJson;
+          this.fjData.fjurl = this.fjData.fpjson.getLocalFilePath(this.file.dataDirectory);
+          this.fjData.ui = this.currentuser;
+          this.fjData.members = this.members;
+
+          this.file.copyFile(imgFileDir, fileName, this.file.dataDirectory + cacheFilePathJson.getCacheDir(), newFileName).then(_ => {
+            this.saveFile();
+          });
+        }
+      });
     }, (err) => {
       console.info("拍照上传附件异常，异常信息：" + err);
     });
