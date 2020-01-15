291aa7c784f1f07f967288643fc6bfce6fe1a424
zhangjy <jy-zhang@zakj.info>
2020-01-15 18:42:47 +0800

    语音修改

--- BaiduSpeechAndTTS/src/ios/XjBaiduWakeUp.m
+++ BaiduSpeechAndTTS/src/ios/XjBaiduWakeUp.m
@@ -40,41 +40,48 @@
 #pragma mark Initialization functions
 
 - (void) pluginInitialize {
-    [self configWakeup];
+    //[self configWakeup];
+
+    self.wakeupEventManager = [BDSEventManager createEventManagerWithName:BDS_WAKEUP_NAME];
 }
 
 - (void)configWakeup {
-    
-    self.wakeupEventManager = [BDSEventManager createEventManagerWithName:BDS_WAKEUP_NAME];
+
     // 设置语音唤醒代理
     [self.wakeupEventManager setDelegate:self];
-   
-     // 参数配置：唤醒语言模型文件路径, 默认文件名为 bds_easr_basic_model.dat bds_easr_dnn_wakeup_model
-     NSString* dat = [[NSBundle mainBundle] pathForResource:@"bds_easr_basic_model" ofType:@"dat"];
-    [self.wakeupEventManager setParameter:dat forKey:BDS_WAKEUP_DAT_FILE_PATH];
      // 参数配置：离线授权APPID
     [self.wakeupEventManager setParameter:@"15388608" forKey:BDS_WAKEUP_APP_CODE];
-
+   
+     // 参数配置：唤醒语言模型文件路径, 默认文件名为 bds_easr_basic_model.dat bds_easr_dnn_wakeup_model
+    NSString* dat = [[NSBundle mainBundle] pathForResource:@"bds_easr_basic_model" ofType:@"dat"];
     // 默认的唤醒词为"百度一下"，如需自定义唤醒词，请在 http://ai.baidu.com/tech/speech/wake 中评估并下载唤醒词，替换此参数
     NSString* words = [[NSBundle mainBundle] pathForResource:@"bds_easr_wakeup_words" ofType:@"dat"];
-    [self.wakeupEventManager setParameter:words forKey:BDS_WAKEUP_WORDS_FILE_PATH];
-    
 
+    [self.wakeupEventManager setParameter:dat forKey:BDS_WAKEUP_DAT_FILE_PATH];
+
+    [self.wakeupEventManager setParameter:words forKey:BDS_WAKEUP_WORDS_FILE_PATH];
+
+    [self.wakeupEventManager setParameter:nil forKey:BDS_WAKEUP_AUDIO_FILE_PATH];
+    [self.wakeupEventManager setParameter:nil forKey:BDS_WAKEUP_AUDIO_INPUT_STREAM];
 }
 
 - (void)start:(CDVInvokedUrlCommand*)command
-{
+
+    [self configWakeup];
+
     self.callbackId = command.callbackId;
-    
     // 发送指令：加载语音唤醒引擎
     [self.wakeupEventManager sendCommand:BDS_WP_CMD_LOAD_ENGINE];
     [self.wakeupEventManager sendCommand:BDS_WP_CMD_START];
+{
+
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command
 {
     self.callbackId = command.callbackId;
     [self.wakeupEventManager sendCommand:BDS_WP_CMD_STOP];
+    [self.wakeupEventManager sendCommand:BDS_WP_CMD_UNLOAD_ENGINE];
 }
 
 - (void)release:(CDVInvokedUrlCommand*)command
